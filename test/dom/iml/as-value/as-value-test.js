008070591910e072171b48ee3c84dc5932fa0f59
Joe Grund <joe.grund@intel.com>
2015-08-19 10:55:07 -0400

    HYD-4726 Add as-value directive
    There are directives that expect a value as input. When using a
    directive like this inside an ng-repeat, it needs a value cooresponding
    to each row displayed.
    
    Given we have a stream, we can create a property. This property can be
    combined with an optional through stream that will transform the final
    value. Once the value is optionally transformed, it will be emitted on
    the transcluded scope. This will allow direct assignment into a
    directive.
    
      - Add as-value directive.
      - Add as-value tests.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

---
+++ test/dom/iml/as-value/as-value-test.js
@@ -0,0 +1,47 @@
+describe('As value', function () {
+  'use strict';
+
+  beforeEach(module('asValue'));
+
+  var $compile, $scope, el, s, getText;
+
+  beforeEach(inject(function ($rootScope, _$compile_) {
+    $compile = _$compile_;
+
+    var template = '<div as-value stream="stream">\
+      <span class="num" >{{ curr.val }}</span>\
+    </div>';
+
+    $scope = $rootScope.$new();
+    s = highland();
+    $scope.stream = s;
+
+    $scope.setNum = function setNum (s) {
+      s.each(function (x) {
+        $scope.num = x;
+      });
+    };
+
+    el = $compile(template)($scope);
+    $scope.$digest();
+
+    var find = el[0].querySelector.bind(el[0]);
+    getText = fp.flow(find, fp.lensProp('textContent'));
+  }));
+
+  it('should be empty to start', function () {
+    expect(getText('.num')).toEqual('');
+  });
+
+  it('should output num', function () {
+    s.write(1);
+    expect(getText('.num')).toEqual('1');
+  });
+
+  it('should output a stream of nums', function () {
+    s.write(1);
+    s.write(2);
+    s.end();
+    expect(getText('.num')).toEqual('2');
+  });
+});
