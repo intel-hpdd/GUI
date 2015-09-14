fb671c66b466f318747682449544c9254aaa9af3
Joe Grund <joe.grund@intel.com>
2015-09-14 10:43:47 -0400

    HYD-4830 Create as-stream directive
    Now that action-dropdown is going to use a stream, we need a val to
    convert singular values into a stream at runtime.
    
    Create as-stream to achieve this. as-stream is the dual of as-value.
    It will be given a $scope and will watch the val property on that scope.
    It will take any changed value and push it into a writable stream it
    creates.
    It will expose the stream as str on a transcluded scope.
    
      - Create as-stream directive.
      - Create DOM tests to cover common usage.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

---
+++ test/dom/iml/as-stream/as-stream-test.js
@@ -0,0 +1,64 @@
+describe('As stream', function () {
+  var s;
+
+  beforeEach(module('asStream', 'asValue', function ($provide) {
+    $provide.value('λ', function () {
+      s = highland();
+
+      spyOn(s, 'destroy');
+
+      return s;
+    });
+  }));
+
+  var $scope, $rootScope, el, getText, compile;
+
+  beforeEach(inject(function (_$rootScope_, $compile) {
+    $rootScope = _$rootScope_;
+
+    var template = '<div as-stream val="val">\
+      <span class="txt" as-value stream="str">\
+        {{ curr.val }}\
+      </span>\
+    </div>';
+
+    $scope = $rootScope.$new();
+    $scope.val = 'foo';
+
+    compile = function compiler ($scope) {
+      var el = $compile(template)($scope);
+      $scope.$digest();
+
+      return el;
+    };
+
+    el = compile($scope);
+
+    var find = el[0].querySelector.bind(el[0]);
+    getText = fp.flow(find, fp.lensProp('textContent'), fp.invokeMethod('trim', []));
+  }));
+
+  it('should throw if str is already on scope', function () {
+    $scope = $rootScope.$new();
+    $scope.str = 'food';
+
+    expect(compile.bind(null, $scope))
+      .toThrow(new Error('str already set on transcluded scope.'));
+  });
+
+  it('should be foo to start', function () {
+    expect(getText('.txt')).toEqual('foo');
+  });
+
+  it('should update to bar', function () {
+    $scope.val = 'bar';
+    $scope.$digest();
+    expect(getText('.txt')).toEqual('bar');
+  });
+
+  it('should destroy the stream on scope destruction', function () {
+    $scope.$destroy();
+
+    expect(s.destroy).toHaveBeenCalledOnce();
+  });
+});
