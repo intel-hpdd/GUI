894c9383f173f36aa8f1249b80500f2f254dcab2
Joe Grund <joe.grund@intel.com>
2015-10-16 10:28:59 -0400

    HYD-5006 Create page-visibility module
    Create a module that utilizes the page visibility API to create and
    destroy an input stream dynamically as the page goes from foreground to
    background.
    
    This will be done by returning an empty passthrough stream that gets
    populated by data from the input stream when the page is visible.
    When the page is hidden, a predefined empty value is pushed through the
    stream to flush it.
    
      - Add module to start/stop a stream when visibility changes.
      - Add tests.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

---
+++ test/spec/iml/stream-when-visible/stream-when-visible-test.js
@@ -0,0 +1,145 @@
+import {streamWhenVisible as streamWhenVisibleFactory}
+  from '../../../../source/chroma_ui/iml/stream-when-visible/stream-when-visible-exports';
+
+describe('stream when visible', () => {
+  var $document, pageVisibility, removeListener, streamWhenVisible,
+    visibilityStream, streamFn, inStream, stream, spy;
+
+  beforeEach(() => {
+    $document = {
+      hidden: false
+    };
+
+    removeListener = jasmine.createSpy('removeListener');
+    pageVisibility = jasmine.createSpy('pageVisibility')
+      .andReturn(removeListener);
+
+    spy = jasmine.createSpy('spy');
+
+    streamWhenVisible = streamWhenVisibleFactory([$document], () => {
+      visibilityStream = highland();
+      spyOn(visibilityStream, 'destroy');
+
+      return visibilityStream;
+    }, pageVisibility);
+
+    inStream = highland();
+    spyOn(inStream, 'destroy');
+    streamFn = jasmine.createSpy('streamFn')
+      .andReturn(inStream);
+
+    stream = streamWhenVisible(streamFn);
+    spyOn(stream, 'destroy').andCallThrough();
+  });
+
+  it('should be a function', () => {
+    expect(streamWhenVisible)
+      .toEqual(jasmine.any(Function));
+  });
+
+  it('should return a stream', () => {
+    expect(highland.isStream(stream))
+      .toBe(true);
+  });
+
+  it('should pass errors to stream', () => {
+    inStream.write({
+      __HighlandStreamError__: true,
+      error: new Error('boom!')
+    });
+
+    stream
+      .errors(spy)
+      .each(fp.noop);
+
+    expect(spy)
+      .toHaveBeenCalledOnce();
+  });
+
+  it('should pass values to stream', () => {
+    inStream.write('foo');
+
+    stream
+      .each(spy);
+
+    expect(spy)
+      .toHaveBeenCalledOnceWith('foo');
+  });
+
+  it('should not write if document is hidden', () => {
+    $document.hidden = true;
+
+    inStream.write('foo');
+
+    stream
+      .each(spy);
+
+    expect(spy)
+      .not
+      .toHaveBeenCalledOnceWith('foo');
+  });
+
+  describe('when hidden', () => {
+    beforeEach(() => {
+      $document.hidden = true;
+
+      inStream = highland();
+
+      streamFn = jasmine.createSpy('streamFn')
+        .andReturn(inStream);
+      stream = streamWhenVisible(streamFn);
+
+      inStream.write('foo');
+    });
+
+    it('should return a stream', () => {
+      expect(highland.isStream(stream))
+        .toBe(true);
+    });
+
+    it('should not call the stream fn', () => {
+      expect(streamFn).not.toHaveBeenCalled();
+    });
+
+    it('should not write data', () => {
+      stream
+        .each(spy);
+
+      expect(spy)
+        .not
+        .toHaveBeenCalledOnceWith('foo');
+    });
+
+    describe('then shown', () => {
+      beforeEach(() => {
+        pageVisibility.mostRecentCall.args[1]();
+      });
+
+      it('should write data', () => {
+        expect(spy);
+      });
+
+      it('should call the stream function', () => {
+        expect(streamFn).toHaveBeenCalledOnce();
+      });
+    });
+  });
+
+  describe('on destroy', () => {
+    beforeEach(() => {
+      stream.destroy();
+    });
+
+    it('should destroy the stream', () => {
+      expect(inStream.destroy).toHaveBeenCalledOnce();
+    });
+
+    it('should destroy the visibleStream', () => {
+      expect(stream.destroy).toHaveBeenCalledOnce();
+    });
+
+    it('should remove the listener', () => {
+      expect(removeListener).toHaveBeenCalledOnce();
+    });
+  });
+});
