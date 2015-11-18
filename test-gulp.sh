ef5604ac733d20565f62f8d7cd1e2163443296ea
Wayne Kenneth Seymour III <wayne.k.seymour.iii@intel.com>
2015-11-18 11:00:12 -0500

    HYD-5100 Add build script testing to gui
    After the switch to Intel Github (at least) we are no longer running the
    build script's own tests.
    
    Add them to the gui so that they are run in CI as well.
    
     - Added neededs deps of the build system to package.json.
     - Updated run_test.sh to include running the build system's tests, and
       archiving the result.
    
    Signed-off-by: Wayne Kenneth Seymour III <wayne.k.seymour.iii@intel.com>

---
+++ test-gulp.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+pushd gulp
+JASMINE_CONFIG_PATH=jasmine.json NODE_ENV=test RUNNER=CI ../node_modules/.bin/jasmine
+popd
+mv gulp/gulp-results.xml ./test-results
+mkdir -p ../results
+mv ./test-results/*.xml ../results
