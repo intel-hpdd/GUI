0a28b83d542e593f003a5a79fb9e280f7ede3bdf
Joe Grund <joe.grund@intel.com>
2015-12-16 14:23:59 -0500

    HYD-5222 GUI: Create axis directive
    Create an axis directive that wraps d3.svg.axis. It will be transcluded
    inside a charter directive and will be able to access it's controller to
    perform updates and calculations.
    
      - Add axis directive.
      - Add tests.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

---
+++ source/chroma_ui/iml/charting/types/axis/axis-directive-exports.js
@@ -0,0 +1,59 @@
+//
+// INTEL CONFIDENTIAL
+//
+// Copyright 2013-2015 Intel Corporation All Rights Reserved.
+//
+// The source code contained or described herein and all documents related
+// to the source code ("Material") are owned by Intel Corporation or its
+// suppliers or licensors. Title to the Material remains with Intel Corporation
+// or its suppliers and licensors. The Material contains trade secrets and
+// proprietary and confidential information of Intel or its suppliers and
+// licensors. The Material is protected by worldwide copyright and trade secret
+// laws and treaty provisions. No part of the Material may be used, copied,
+// reproduced, modified, published, uploaded, posted, transmitted, distributed,
+// or disclosed in any way without Intel's prior express written permission.
+//
+// No license under any patent, copyright, trade secret or other intellectual
+// property right is granted to or conferred upon you by disclosure or delivery
+// of the Materials, either expressly, by implication, inducement, estoppel or
+// otherwise. Any license under such intellectual property rights must be
+// express and approved by Intel in writing.
+
+export function axisDirective (d3) {
+  'ngInject';
+
+  return {
+    restrict: 'A',
+    scope: {
+      scale: '=',
+      orient: '='
+    },
+    require: '^^charter',
+    templateNamespace: 'svg',
+    template: `<g class="axis"></g>`,
+    link (scope, el, attrs, chartCtrl) {
+      const axis = d3.svg.axis()
+        .scale(scope.scale)
+        .orient(scope.orient);
+
+      const translateBottom = (a) => a.attr('transform', `translate(0,${chartCtrl.getHeight() - 20})`);
+
+      const translate = fp.cond(
+        [fp.eq('bottom'), fp.always(translateBottom)],
+        [fp.always(true), fp.always(fp.identity)]
+      );
+
+      const axisEl = d3.select(el[0])
+        .select('.axis')
+        .call(translate(scope.orient));
+
+      const updateAxis = ({svg}) => svg.each(() => {
+        axisEl
+          .transition()
+          .call(axis);
+      });
+
+      chartCtrl.onUpdate.push(updateAxis);
+    }
+  };
+}
