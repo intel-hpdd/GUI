8d5bdef7a13a38f71858eae03849088c022536c9
Joe Grund <joe.grund@intel.com>
2015-12-17 08:55:59 -0500

    HYD-5223 GUI: Create label model
    Create a label model that can be used to add / remove / update a SVG
    label
    (rect + text) in a generic way.
    
    The model will have a data accessor. This means we can dynamically add /
    remove the label (with transitions) whenenver data changes.
    
      - Add label model.
      - Add tests.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

---
+++ test/dom/iml/charting/types/label/get-label-exports-test.js
@@ -0,0 +1,170 @@
+import {getLabelFactory} from
+  '../../../../../../source/chroma_ui/iml/charting/types/label/get-label-exports';
+
+describe('get label', () => {
+  var d3, getLabel, svg, label;
+
+  beforeEach(module('d3'));
+
+  beforeEach(inject(function (_d3_) {
+    d3 = _d3_;
+
+    getLabel = getLabelFactory(d3);
+
+    svg = document
+      .createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('width', 500);
+    svg.setAttribute('height', 500);
+
+    label = getLabel();
+  }));
+
+  it('should be a function', () => {
+    expect(getLabel).toEqual(jasmine.any(Function));
+  });
+
+  it('should have a color getter', () => {
+    expect(label.color()).toBe('#000000');
+  });
+
+  it('should have a color setter', () => {
+    label.color('#FFFFFF');
+
+    expect(label.color()).toBe('#FFFFFF');
+  });
+
+  it('should have a fill getter', () => {
+    expect(label.fill()).toBe('#FFFFFF');
+  });
+
+  it('should have a fill setter', () => {
+    label.fill('#000000');
+
+    expect(label.fill()).toBe('#000000');
+  });
+
+  it('should have a width getter', () => {
+    expect(label.width()).toBe(0);
+  });
+
+  it('should have a width setter', () => {
+    label.width(100);
+
+    expect(label.width()).toBe(100);
+  });
+
+  it('should have a height getter', () => {
+    expect(label.height()).toBe(0);
+  });
+
+  it('should have a height setter', () => {
+    label.height(100);
+
+    expect(label.height()).toBe(100);
+  });
+
+  it('should have a data getter', () => {
+    expect(label.data()).toBe(fp.identity);
+  });
+
+  it('should have a data setter', () => {
+    label.data(['x']);
+
+    expect(label.data()).toEqual(['x']);
+  });
+
+  describe('rendering', () => {
+    var qs;
+
+    beforeEach(() => {
+      label.data(() => ['data string']);
+      label.fill('#111111');
+      label.width(500);
+      label.height(400);
+
+      document.body.appendChild(svg);
+
+      d3.select(svg)
+        .datum([1, 2, 3, 4, 5, 6])
+        .call(label);
+
+      qs = fp.flow(
+        fp.arrayWrap,
+        fp.invokeMethod('querySelector', fp.__, svg)
+      );
+    });
+
+    afterEach(() => {
+      document.body.removeChild(svg);
+    });
+
+    it('should append a rect', () => {
+      expect(qs('.label-rect')).not.toBeNull();
+    });
+
+    it('should set fill on the rect', () => {
+      expect(qs('.label-rect').getAttribute('fill'))
+        .toBe('#111111');
+    });
+
+    it('should set height on the rect', () => {
+      expect(qs('.label-rect').getAttribute('height'))
+        .toBe('400');
+    });
+
+    it('should set width on the rect', () => {
+      expect(qs('.label-rect').getAttribute('width'))
+        .toBe('500');
+    });
+
+    it('should append text', () => {
+      expect(qs('.label-text')).not.toBeNull();
+    });
+
+    it('should set text content', () => {
+      expect(qs('.label-text').textContent)
+        .toBe('data string');
+    });
+
+    it('should set text x position', () => {
+      expect(qs('.label-text').getAttribute('x'))
+        .toBe('250');
+    });
+
+    it('should set text y position', () => {
+      window.flushD3Transitions();
+
+      expect(qs('.label-text').getAttribute('y'))
+        .toBe('209');
+    });
+
+    it('should set text anchor to middle', () => {
+      expect(qs('.label-text').style['text-anchor'])
+        .toBe('middle');
+    });
+
+    it('should set the fill', () => {
+      expect(qs('.label-text').getAttribute('fill'))
+        .toBe('#000000');
+    });
+
+    describe('with no data', () => {
+      beforeEach(() => {
+        label.data(() => []);
+
+        d3.select(svg)
+          .call(label);
+
+        window.flushD3Transitions();
+      });
+
+      it('should remove the rect', () => {
+        expect(qs('.label-rect')).toBeNull();
+      });
+
+      it('should remove the text', () => {
+        expect(qs('.label-text')).toBeNull();
+      });
+    });
+  });
+});
