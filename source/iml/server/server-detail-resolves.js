bdad7b6badcbf5f49884a252bc251dfdd4733038
Will Johnson <william.c.johnson@intel.com>
2016-04-04 15:55:26 -0400

    HYD-5775 Update host call to use the store in server-detail-resolves
    Now that the call to /hosts is being stored in the store, we should no
    longer establish a socket connection to /hosts in
    server-detail-resolves. Instead, host data should be retrieved from the
    store.
    
    This patch will update server-detail-resolves such that a socket
    connection is no longer used to retrieve host data. Instead, data will
    be pulled from the store in real time.
    
    - server-detail.html - Removed comments
    - server-detail-resolves.js - Update the socket call to /hosts to
      retrieve data from the store and filter on the route id
    - server-detail-resolves-test.js - Update tests to ensure we pull server
      data from the store, remove socket call test, and ensure that the data
      is filtered correctly
    
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- source/iml/server/server-detail-resolves.js
+++ source/iml/server/server-detail-resolves.js
@@ -46,10 +46,12 @@ export default function serverDetailResolvesFactory ($q, resolveStream, addPrope
         .select('alertIndicators')
     );
 
-    const serverStream = resolveStream(socketStream('/host/' + $route.current.params.id, {
-      jsonMask: 'available_actions,resource_uri,address,fqdn,nodename,install_method,\
-server_profile(ui_name,managed,initial_state),boot_time,state_modified_at,id,member_of_active_filesystem,locks,state'
-    }));
+    const serverStream = rebindDestroy(
+      fp.map(fp.identity),
+      getStore
+        .select('server')
+        .map(fp.find(x => x.id === $route.current.params.id))
+    );
 
     var allHostMatches = {
       qs: {
