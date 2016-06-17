cf72d053cfc45d6fd2024becbc18a91425ad71a2
Will Johnson <william.c.johnson@intel.com>
2016-06-17 14:58:57 -0400

    Add chart reducers for mdo, read write heat map, and ost fixes #277
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

---
+++ test/spec/iml/ost-balance/ost-balance-chart-reducer-test.js
@@ -0,0 +1,46 @@
+import {
+  ADD_OST_BALANCE_CHART_ITEMS,
+  default as ostBalanceChartReducer
+} from '../../../../source/iml/ost-balance/ost-balance-chart-reducer.js';
+import deepFreeze from 'intel-deep-freeze';
+
+describe('server reducer', () => {
+
+  it('should be a function', () => {
+    expect(ostBalanceChartReducer).toEqual(jasmine.any(Function));
+  });
+
+  describe('matching type', () => {
+    it('should return the payload', () => {
+      expect(ostBalanceChartReducer({
+        '': {
+          percentage: 10,
+          page: ''
+        }
+      }, {
+        type: ADD_OST_BALANCE_CHART_ITEMS,
+        payload: [{
+          percentage: 85,
+          page: '8'
+        }]
+      })).toEqual({
+        '': {
+          percentage: 10,
+          page: ''
+        }, '8': {
+          percentage: 85,
+          page: '8'
+        }
+      });
+    });
+  });
+
+  describe('non-matching type', () => {
+    it('should return the state', () => {
+      expect(ostBalanceChartReducer(deepFreeze([]), {
+        type: 'ADD_ALERT_INDICATOR_ITEMS',
+        payload: {key: 'val'}
+      })).toEqual([]);
+    });
+  });
+});
---
+++ test/spec/iml/read-write-heat-map/read-write-heat-map-chart-reducer-test.js
@@ -0,0 +1,66 @@
+import {
+  ADD_READ_WRITE_HEAT_MAP_CHART_ITEMS,
+  default as readWriteHeatMapChartReducer
+} from '../../../../source/iml/read-write-heat-map/read-write-heat-map-chart-reducer.js';
+import deepFreeze from 'intel-deep-freeze';
+
+describe('server reducer', () => {
+
+  it('should be a function', () => {
+    expect(readWriteHeatMapChartReducer).toEqual(jasmine.any(Function));
+  });
+
+  describe('matching type', () => {
+    it('should return the payload', () => {
+      expect(readWriteHeatMapChartReducer({
+        '': {
+          dataType: 'dataType',
+          configType: 'range',
+          startDate: 'startDate',
+          endDate: 'endDate',
+          size: 10,
+          unit: 'minutes',
+          page: ''
+        }
+      }, {
+        type: ADD_READ_WRITE_HEAT_MAP_CHART_ITEMS,
+        payload: [{
+          dataType: 'dataType',
+          configType: 'duration',
+          startDate: 'startDate',
+          endDate: 'endDate',
+          size: 3,
+          unit: 'hours',
+          page: 'target8'
+        }]
+      })).toEqual({
+        '': {
+          dataType: 'dataType',
+          configType: 'range',
+          startDate: 'startDate',
+          endDate: 'endDate',
+          size: 10,
+          unit: 'minutes',
+          page: ''
+        }, 'target8': {
+          dataType: 'dataType',
+          configType: 'duration',
+          startDate: 'startDate',
+          endDate: 'endDate',
+          size: 3,
+          unit: 'hours',
+          page: 'target8'
+        }
+      });
+    });
+  });
+
+  describe('non-matching type', () => {
+    it('should return the state', () => {
+      expect(readWriteHeatMapChartReducer(deepFreeze([]), {
+        type: 'ADD_ALERT_INDICATOR_ITEMS',
+        payload: {key: 'val'}
+      })).toEqual([]);
+    });
+  });
+});
