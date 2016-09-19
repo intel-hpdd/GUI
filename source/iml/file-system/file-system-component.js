e92b37072145e6520f8338f9b93a137bdc9c1ebd
Grund, Joe <joe.grund@intel.com>
2016-09-19 11:12:37 -0400

    Update file-system-component.js

--- source/iml/file-system/file-system-component.js
+++ source/iml/file-system/file-system-component.js
@@ -37,7 +37,7 @@ export default {
         <tr>
           <th>File System</th>
           <th>Status</th>
-          <th>Primary Management Server</th>
+          <th>Primary MGS</th>
           <th>Metadata Server Count</th>
           <th>Connected Clients</th>
           <th>Space Used / Total</th>
