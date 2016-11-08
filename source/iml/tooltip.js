8c3b4a789b4fffbdd3ddb7a8a9b3ccbc043bc63c
Will Johnson <william.c.johnson@intel.com>
2016-11-08 14:23:00 -0500

    HYD-6805 Create Inferno Tooltip
    Update the tooltip to use inferno instead of using angular-ui-bootstrap.
    
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

---
+++ source/iml/tooltip.js
@@ -0,0 +1,51 @@
+// @flow
+
+//
+// INTEL CONFIDENTIAL
+//
+// Copyright 2013-2016 Intel Corporation All Rights Reserved.
+//
+// The source code contained or described herein and all documents related
+// to the source code ("Material") are owned by Intel Corporation or its
+// suppliers or licensors. Title to the Material remains with Intel Corporation
+// or its suppliers and licensors. The Material contains trade secrets and
+// proprietary and confidential information of Intel or its suppliers and
+// licensors. The Material is protected by worldwide copyright and trade secret
+// laws and treaty provisions. No part of the Material may be used, copied,
+// reproduced, modified, published, uploaded, posted, transmitted, distributed,
+// or disclosed in any way without Intel's prior express written permission.
+//
+// No license under any patent, copyright, trade secret or other intellectual
+// property right is granted to or conferred upon you by disclosure or delivery
+// of the Materials, either expressly, by implication, inducement, estoppel or
+// otherwise. Any license under such intellectual property rights must be
+// express and approved by Intel in writing.
+
+
+import Inferno from 'inferno';
+
+type directionsT =
+  | 'left'
+  | 'right'
+  | 'top'
+  | 'bottom';
+
+type tooltipT = {
+  message?:string,
+  direction:directionsT,
+  moreClasses?:string[]
+};
+
+const TooltipComponent = ({message, direction, moreClasses}:tooltipT) => {
+  if (!message)
+    return;
+
+  return (<div className={`tooltip inferno-tt ${direction} ${moreClasses ? moreClasses.join(' ') : ''}`}>
+    <div class="tooltip-arrow"></div>
+    <div class="tooltip-inner">
+      <span>{message}</span>
+    </div>
+  </div>);
+};
+
+export default TooltipComponent;
