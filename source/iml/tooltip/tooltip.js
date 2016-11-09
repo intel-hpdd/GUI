d828c39b4ed778137cb3b6eef54f2433f8a31106
Joe Grund <joe.grund@intel.com>
2016-11-09 12:16:28 -0500

    HYD-6811 Replace iml-tooltip with simpler implementation
    Update the iml-tooltip to use a simpler static positioning
    implementation.
    
    We should be able to drop a good amount of code.
    
      - Refactor iml-tooltip.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- source/iml/tooltip/tooltip.js
+++ source/iml/tooltip/tooltip.js
@@ -1,3 +1,5 @@
+// @flow
+
 //
 // INTEL CONFIDENTIAL
 //
@@ -20,14 +22,23 @@
 // express and approved by Intel in writing.
 
 import angular from 'angular';
-import _ from 'intel-lodash-mixins';
 import Inferno from 'inferno';
 import InfernoDOM from 'inferno-dom';
 import HelpTooltip from '../help-tooltip.js';
 
 import tooltipTemplate from './assets/html/tooltip.html!text';
 
-export function imlTooltip (position, $timeout, $$rAF, strategies) {
+import type {
+  $scopeT
+} from 'angular';
+
+import type {
+  directionsT
+} from '../tooltip.js';
+
+type strategiesT = (jqElement:HTMLElement[], scope:$scopeT, events:Object) => () => mixed;
+
+export function imlTooltip (strategies:strategiesT) {
   'ngInject';
 
   return {
@@ -39,125 +50,51 @@ export function imlTooltip (position, $timeout, $$rAF, strategies) {
     transclude: true,
     replace: true,
     template: tooltipTemplate,
-    link: function link (scope, jqElement) {
+    link (scope:$scopeT & {in:string}, jqElement:HTMLElement[]) {
       let deregister;
 
-      let jqPreviousEl = jqElement.prev();
-
-      const positioner = position.positioner(jqElement[0]);
+      const el = jqElement[0];
+      let previousEl = el.previousElementSibling;
 
       // look at the parent's previous sibling
-      if (!jqPreviousEl.length)
-        jqPreviousEl = jqElement.parent().prev();
+      if (!previousEl) {
+        const parentEl = el.parentNode;
 
-      if (!jqPreviousEl.length)
+        if (parentEl)
+          previousEl = parent.previousElementSibling;
+      }
+
+      if (!previousEl)
         throw new Error('Previous element not found for tooltip!');
 
-      const directions = _.values(position.DIRECTIONS);
-      const directionsJoined = directions.join(' ');
-
-      directions.splice(directions.indexOf(scope.direction), 1);
-      directions.unshift(scope.direction);
-
-      scope.$on('$destroy', function onDestroy () {
-        jqPreviousEl = null;
+      scope.$on('$destroy', () => {
+        previousEl = null;
         deregister();
       });
 
       if (!scope.hasOwnProperty('toggle'))
-        deregister = strategies(jqPreviousEl, scope, {
-          show: turnThenShow,
-          hide: hide
-        });
+        deregister = strategies(
+          angular.element(previousEl),
+          scope,
+          {
+            show,
+            hide
+          }
+        );
       else
-        deregister = scope.$watch('toggle', function (newValue) {
+        deregister = scope.$watch('toggle', newValue => {
           if (newValue)
-            turnThenShow();
+            show();
           else
             hide();
         }, true);
 
-      scope.$watch(
-        function setWatch () {
-          return jqElement.html();
-        },
-        function handleChange (newValue, oldValue) {
-          if (newValue !== oldValue)
-            $$rAF(recalculate);
-        }
-      );
-
-      /**
-       * Figures out the placement of the popover and applies it to the element's style.
-       */
-      function recalculate () {
-        jqElement.css('min-width', '');
-        jqElement.css(position.position(scope.direction, positioner));
-      }
-
-      function turnThenShow () {
-        $timeout(show);
-      }
-
       function show () {
-        setPosition();
-
-        angular.element(position.$window).on('resize', throttledResize);
+        scope.in = 'in';
       }
 
       function hide () {
         delete scope.in;
-
-        angular.element(position.$window).off('resize', throttledResize);
-      }
-
-      const throttledResize = _.throttle(function onThrottle () {
-        if (!jqElement.hasClass('in')) return;
-
-        setPosition();
-        scope.$digest();
-      }, 500);
-
-      /**
-       * Sets the position of the tooltip by placing a clone until a match is found.
-       */
-      function setPosition () {
-        let clone = jqElement[0].cloneNode(true);
-        let jqClone = angular.element(clone);
-
-        const clonePosition = position.positioner(clone);
-        const windowPosition = position.positioner(position.$window);
-
-        jqClone.css('display', 'block');
-
-        // place the clone.
-        jqElement.after(clone);
-
-        // check where clone fits.
-        directions.some(function (direction) {
-          delete scope.in;
-
-          jqClone.removeClass(directionsJoined).addClass(direction);
-
-          const calculatedClonePosition = position.position(direction, clonePosition);
-
-          _.extend(clone.style, calculatedClonePosition);
-
-          const fits = !position.overflows(direction, windowPosition, clonePosition);
-
-          if (fits) {
-            scope.placement = direction;
-            scope.in = 'in';
-            _.extend(jqElement[0].style, calculatedClonePosition);
-          }
-
-          return fits;
-        });
-
-        //destroy the clone
-        jqClone.remove();
-        clone = null;
-        jqClone = null;
       }
     }
   };
@@ -172,7 +109,7 @@ export function helpTooltip () {
       direction: '@'
     },
     restrict: 'E',
-    link: function link (scope, el) {
+    link: function link (scope:{|topic:string, direction:directionsT|}, el:HTMLElement[]) {
       InfernoDOM.render(
         <HelpTooltip helpKey={scope.topic} direction={scope.direction} />,
         el[0]
