f64ac3e2e1f222d8c01828b7f2458cbd64633c42
Joe Grund <joe.grund@intel.com>
2016-11-14 16:21:05 -0500

    HYD-6829 GUI: Create connect for inferno components
    Create the ability to wire a highland stream to a inferno component,
    using a higher-order component.
    
    This will be similar to how the `as-value` directive functions.
    
      - Create connect function.
      - Add tests.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- flow-typed/inferno-component.js
+++ flow-typed/inferno-component.js
@@ -1,12 +1,5 @@
 // @flow
 
-declare class Inferno$Component<DefaultProps, Props, State> extends React$Component<DefaultProps, Props, State> {
-  static defaultProps:DefaultProps;
-  props:Props;
-  state:State;
-}
-
-
 declare module 'inferno-component' {
-  declare export default typeof Inferno$Component
+  declare export default typeof React$Component
 }
