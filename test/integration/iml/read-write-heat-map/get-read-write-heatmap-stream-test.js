34f201203aa837c419dc1ffc0596c20ccf65071f
Will Johnson <william.c.johnson@intel.com>
2017-03-22 18:15:03 -0400

    HYD-7278 Update GUI to use read-write-heat-map route in socket worker
    Now that socket worker has been updated to include a route where
    read-write-heat-map data munging will take place, update the GUI such
    that it delegates data processing to the web worker.
    
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- test/integration/iml/read-write-heat-map/get-read-write-heatmap-stream-test.js
+++ test/integration/iml/read-write-heat-map/get-read-write-heatmap-stream-test.js
@@ -1,192 +1,78 @@
 import highland from 'highland';
-import moment from 'moment';
-import readWriteHeatMapDataFixtures
-  from '../../../data-fixtures/read-write-heat-map-fixtures.json!json';
-
-import * as fp from 'intel-fp';
-
-import { clone } from 'intel-obj';
-
 import { mock, resetAll } from '../../../system-mock.js';
 
 describe('the read write heat map stream', () => {
   let socketStream,
-    getServerMoment,
-    streams,
-    bufferDataNewerThan,
-    getRequestDuration;
+    flushOnChange,
+    getReadWriteHeatMapStream,
+    spy,
+    result$,
+    heatMap$,
+    change$;
 
   beforeEachAsync(async function() {
-    streams = {
-      heatMap: [],
-      target: []
-    };
-
-    socketStream = jasmine.createSpy('socketStream').and.callFake(path => {
-      const s = highland();
-
-      if (path === '/target/metric') streams.heatMap.push(s);
-      else if (path === '/target') streams.target.push(s);
-
-      return s;
-    });
-
-    getServerMoment = jasmine
-      .createSpy('getServerMoment')
-      .and.returnValue(moment('2014-01-07T14:42:50+00:00'));
-
-    await mock('source/iml/charting/union-with-target.js', {
-      'source/iml/socket/socket-stream.js': { default: socketStream },
-      'source/iml/get-server-moment.js': { default: getServerMoment }
-    });
-
-    const bufferDataNewerThanModule = await mock(
-      'source/iml/charting/buffer-data-newer-than.js',
-      {}
-    );
-    bufferDataNewerThan = bufferDataNewerThanModule.default;
-
-    const getTimeParamsModule = await mock(
-      'source/iml/charting/get-time-params.js',
-      {
-        'source/iml/create-date': {
-          default: () => new Date()
-        }
-      }
-    );
-    getRequestDuration = getTimeParamsModule.getRequestDuration;
+    heatMap$ = highland();
+    change$ = highland();
+    socketStream = jasmine.createSpy('socketStream').and.returnValue(heatMap$);
+    flushOnChange = jasmine.createSpy('flushOnChange').and.returnValue(change$);
+    spy = jasmine.createSpy('spy');
 
     const mod = await mock(
       'source/iml/read-write-heat-map/get-read-write-heat-map-stream.js',
-      {}
+      {
+        'source/iml/socket/socket-stream.js': { default: socketStream },
+        'source/iml/chart-transformers/chart-transformers.js': { flushOnChange }
+      }
     );
 
     getReadWriteHeatMapStream = mod.default;
   });
 
-  let getReadWriteHeatMapStream, fixtures, spy;
-
   beforeEach(() => {
     spy = jasmine.createSpy('spy');
+    result$ = getReadWriteHeatMapStream(
+      'stats_read_bytes',
+      { foo: 'bar' },
+      { size: 10, unit: 'minutes' },
+      undefined,
+      -275
+    );
+    result$.each(spy);
 
-    fixtures = readWriteHeatMapDataFixtures;
-
-    jasmine.clock().install();
+    change$.write({
+      '1': {
+        ts: '2017-01-01T00:00:00+00:00',
+        data: { stats_read_bytes: 123 }
+      }
+    });
   });
 
   afterEach(() => {
-    jasmine.clock().uninstall();
+    resetAll();
+    result$.destroy();
+    change$.destroy();
   });
 
-  afterEach(resetAll);
-
-  it('should return a factory function', () => {
-    expect(getReadWriteHeatMapStream).toEqual(jasmine.any(Function));
+  it('should call socket stream', () => {
+    expect(socketStream).toHaveBeenCalledOnceWith('/read-write-heat-map', {
+      type: 'stats_read_bytes',
+      durationParams: { size: 10, unit: 'minutes' },
+      rangeParams: undefined,
+      timeOffset: -275,
+      foo: 'bar'
+    });
   });
 
-  describe('fetching 10 minutes ago', () => {
-    let readWriteHeatMapStream;
+  it('should call flushOnChange', () => {
+    expect(flushOnChange).toHaveBeenCalledWith(jasmine.any(Object));
+  });
 
-    beforeEach(() => {
-      const buff = bufferDataNewerThan(10, 'minutes');
-      const requestDuration = getRequestDuration({}, 10, 'minutes');
-
-      readWriteHeatMapStream = getReadWriteHeatMapStream(
-        'stats_read_bytes',
-        requestDuration,
-        buff
-      );
-
-      readWriteHeatMapStream.each(spy);
-    });
-
-    describe('when there is data', () => {
-      beforeEach(() => {
-        streams.target[0].write({ objects: [] });
-        streams.target[0].end();
-        jasmine.clock().tick(10000);
-
-        streams.heatMap[0].write(clone(fixtures[0].in));
-        streams.heatMap[0].end();
-        jasmine.clock().tick(10000);
-      });
-
-      it('should return a stream', () => {
-        expect(highland.isStream(readWriteHeatMapStream)).toBe(true);
-      });
-
-      it('should return computed data', () => {
-        expect(spy).toHaveBeenCalledOnceWith(fixtures[0].out);
-      });
-
-      it('should maintain consistent data', () => {
-        streams.heatMap[1].write({ 1: fixtures[0].in[1] });
-        streams.heatMap[1].end();
-        streams.target[1].write({ objects: [] });
-        streams.target[1].end();
-        jasmine.clock().tick(10000);
-
-        expect(spy).toHaveBeenCalledWith(fixtures[0].out);
-      });
-
-      it('should union with a target', () => {
-        streams.heatMap[1].write({
-          1: fp.view(fp.lensProp(1))(clone(fixtures[0].in))
-        });
-        streams.heatMap[1].end();
-        streams.target[1].write({
-          objects: [
-            {
-              id: '1',
-              name: 'OST001'
-            },
-            {
-              id: '2',
-              name: 'OST002'
-            }
-          ]
-        });
-        streams.target[1].end();
-        jasmine.clock().tick(10000);
-
-        const result = clone(fixtures[0].out);
-
-        result[0][0].name = 'OST001';
-        result[1][0].name = 'OST002';
-
-        expect(spy).toHaveBeenCalledWith(result);
-      });
-    });
-
-    describe('when there is no initial data', () => {
-      beforeEach(() => {
-        streams.heatMap[0].write({});
-        streams.heatMap[0].end();
-        streams.target[0].write({ objects: [] });
-        streams.target[0].end();
-        jasmine.clock().tick(10000);
-      });
-
-      it('should populate if data comes in on next tick', () => {
-        streams.heatMap[1].write({
-          1: fp.view(fp.lensProp(1))(clone(fixtures[0].in))
-        });
-        streams.heatMap[1].end();
-        streams.target[1].write({ objects: [] });
-        streams.target[1].end();
-        jasmine.clock().tick(10000);
-
-        expect(spy).toHaveBeenCalledOnceWith([
-          [
-            {
-              id: '1',
-              name: '1',
-              data: { stats_read_bytes: 7613151815.7 },
-              ts: '2014-01-07T14:42:50+00:00'
-            }
-          ]
-        ]);
-      });
+  it('should pass the data through the stream', () => {
+    expect(spy).toHaveBeenCalledOnceWith({
+      '1': {
+        ts: '2017-01-01T00:00:00+00:00',
+        data: { stats_read_bytes: 123 }
+      }
     });
   });
 });
