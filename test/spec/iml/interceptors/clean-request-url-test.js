b3ed7c1d350b999121cdd7da82387e66b2edc28c
Will Johnson <william.c.johnson@intel.com>
2017-05-16 17:14:52 -0400

    - Fix clean-request-url-test
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- test/spec/iml/interceptors/clean-request-url-test.js
+++ test/spec/iml/interceptors/clean-request-url-test.js
@@ -1,10 +1,11 @@
 import interceptorsModule
   from '../../../../source/iml/interceptors/interceptor-module';
+import angular from '../../../angular-mock-setup.js';
 
 describe('clean request url interceptor', () => {
   let cleanRequestUrlInterceptor;
 
-  beforeEach(module(interceptorsModule));
+  beforeEach(angular.mock.module(interceptorsModule));
 
   beforeEach(
     inject(_cleanRequestUrlInterceptor_ => {
