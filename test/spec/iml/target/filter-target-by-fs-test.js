cd8649b6b482023b31d5baa422b13860fc89a69a
Will Johnson <william.c.johnson@intel.com>
2017-05-16 17:00:25 -0400

    - Fix filter-target-by-fs-test.js
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- test/spec/iml/target/filter-target-by-fs-test.js
+++ test/spec/iml/target/filter-target-by-fs-test.js
@@ -35,7 +35,7 @@ describe('filter target by fs', () => {
   it('should return the targets with the matching fs', function() {
     let result;
 
-    highland(data).through(filterTargetByFs('1')).each(function(x) {
+    highland(data).through(filterTargetByFs(1)).each(function(x) {
       result = x;
     });
 
