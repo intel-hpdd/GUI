0a65413c82caca78c6a07a62c8ef54bc89b33c05
Will Johnson <william.c.johnson@intel.com>
2017-05-27 16:10:30 -0400

    - fix status-query-component-test.js
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- test/spec/iml/status/status-query-component-test.js
+++ test/spec/iml/status/status-query-component-test.js
@@ -1,53 +1,43 @@
 import highland from 'highland';
-
-import { mock, resetAll } from '../../../system-mock.js';
+import angular from '../../../angular-mock-setup.js';
 
 describe('status query controller', () => {
   let ctrl,
     $location,
     qsStream,
     s,
-    statusCompleter,
+    mockStatusCompleter,
     $stateParams,
-    statusInputToQsParser,
-    statusQsToInputParser,
+    mockStatusInputToQsParser,
+    mockStatusQsToInputParser,
     mod;
-
-  beforeEachAsync(async function() {
-    $location = {
-      search: jasmine.createSpy('search')
-    };
-
+  beforeEach(function() {
+    $location = { search: jest.fn() };
     s = highland();
-    spyOn(s, 'destroy');
+    jest.spyOn(s, 'destroy');
+    qsStream = jest.fn(() => s);
+    $stateParams = { param: 'val' };
+    mockStatusCompleter = 'completer';
+    mockStatusQsToInputParser = 'statusQsToInputParser';
+    mockStatusInputToQsParser = 'statusInputToQsParser';
+    jest.mock(
+      '../../../../source/iml/status/status-qs-to-input-parser.js',
+      () => mockStatusQsToInputParser
+    );
+    jest.mock(
+      '../../../../source/iml/status/status-input-to-qs-parser.js',
+      () => mockStatusInputToQsParser
+    );
+    jest.mock(
+      '../../../../source/iml/status/status-completer.js',
+      () => mockStatusCompleter
+    );
 
-    qsStream = jasmine.createSpy('qsStream').and.returnValue(s);
-
-    $stateParams = {
-      param: 'val'
-    };
-
-    statusCompleter = 'completer';
-    statusQsToInputParser = 'statusQsToInputParser';
-    statusInputToQsParser = 'statusInputToQsParser';
-
-    mod = await mock('source/iml/status/status-query-component.js', {
-      'source/iml/status/status-qs-to-input-parser.js': {
-        default: statusQsToInputParser
-      },
-      'source/iml/status/status-input-to-qs-parser.js': {
-        default: statusInputToQsParser
-      },
-      'source/iml/status/status-completer.js': {
-        default: statusCompleter
-      }
-    });
+    mod = require('../../../../source/iml/status/status-query-component.js');
   });
 
-  beforeEach(module('extendScope'));
-
   beforeEach(
-    inject(($rootScope, propagateChange) => {
+    angular.mock.inject(($rootScope, propagateChange) => {
       const $scope = $rootScope.$new();
       ctrl = new mod.StatusQueryController(
         $scope,
@@ -58,43 +48,30 @@ describe('status query controller', () => {
       );
     })
   );
-
-  afterEach(resetAll);
-
   it('should set the controller properties', () => {
     const instance = jasmine.objectContaining({
       parserFormatter: {
-        parser: statusInputToQsParser,
-        formatter: statusQsToInputParser
+        parser: mockStatusInputToQsParser,
+        formatter: mockStatusQsToInputParser
       },
-      completer: statusCompleter,
+      completer: mockStatusCompleter,
       onSubmit: expect.any(Function)
     });
-
     expect(ctrl).toEqual(instance);
   });
-
   it('should call qsStream', () => {
     expect(qsStream).toHaveBeenCalledOnceWith($stateParams);
   });
-
   it('should set the current qs', () => {
-    s.write({
-      qs: 'bar=baz'
-    });
-
+    s.write({ qs: 'bar=baz' });
     expect(ctrl.qs).toBe('bar=baz');
   });
-
   it('should set the location query string on submit', () => {
     ctrl.onSubmit('foo=bar');
-
     expect($location.search).toHaveBeenCalledWith('foo=bar');
   });
-
   it('should destroy the route stream when the scope is destroyed', () => {
     ctrl.$onDestroy();
-
     expect(s.destroy).toHaveBeenCalledTimes(1);
   });
 });
