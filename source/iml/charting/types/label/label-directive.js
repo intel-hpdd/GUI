fc7845a1850490b325cf94e86b3e599c374623fb
Joe Grund <joe.grund@intel.com>
2017-05-29 20:18:58 -0400

    remove dead angular import.
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- source/iml/charting/types/label/label-directive.js
+++ source/iml/charting/types/label/label-directive.js
@@ -20,8 +20,9 @@
 // express and approved by Intel in writing.
 
 import * as fp from '@mfl/fp';
+import d3 from 'd3';
 
-export function labelDirective(d3, getLabel) {
+export function labelDirective(getLabel) {
   'ngInject';
   return {
     restrict: 'A',
