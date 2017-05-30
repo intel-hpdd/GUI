b94f6d9df1d1f4e8f180e76a5b9b02d20ed06aed
Will Johnson <william.c.johnson@intel.com>
2017-05-30 15:32:33 -0400

    - fix lnet-options test
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- test/spec/iml/lnet/lnet-options-test.js
+++ test/spec/iml/lnet/lnet-options-test.js
@@ -1,27 +1,24 @@
-import lnetModule from '../../../../source/iml/lnet/lnet-module';
+import lnetOptions from '../../../../source/iml/lnet/lnet-options.js';
+
+const expectedValues = [
+  { name: 'Not Lustre Network', value: -1 },
+  { name: 'Lustre Network 0', value: 0 },
+  { name: 'Lustre Network 1', value: 1 },
+  { name: 'Lustre Network 2', value: 2 },
+  { name: 'Lustre Network 3', value: 3 },
+  { name: 'Lustre Network 4', value: 4 },
+  { name: 'Lustre Network 5', value: 5 },
+  { name: 'Lustre Network 6', value: 6 },
+  { name: 'Lustre Network 7', value: 7 },
+  { name: 'Lustre Network 8', value: 8 },
+  { name: 'Lustre Network 9', value: 9 }
+];
 
 describe('lnet options', function() {
-  let LNET_OPTIONS;
-
-  const expectedValues = Array.from(Array(11), (x, idx) => idx - 1).map(
-    value =>
-      value === -1
-        ? { name: 'Not Lustre Network', value }
-        : { name: `Lustre Network ${value}`, value }
-  );
-
-  beforeEach(module(lnetModule));
-
-  beforeEach(
-    inject(function(_LNET_OPTIONS_) {
-      LNET_OPTIONS = _LNET_OPTIONS_;
-    })
-  );
-
   describe('enum', () => {
     expectedValues.forEach((expectedValue, index) => {
       it(`should have a value for ${expectedValue.name}.`, () => {
-        expect(LNET_OPTIONS[index]).toEqual(expectedValue);
+        expect(lnetOptions[index]).toEqual(expectedValue);
       });
     });
   });
