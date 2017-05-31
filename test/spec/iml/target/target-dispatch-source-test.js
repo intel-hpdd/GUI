0e98e7cf7b65c302bba7be5203815f6851b80498
Will Johnson <william.c.johnson@intel.com>
2017-05-30 21:38:12 -0400

    - fix target-dispatch-source test
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- test/spec/iml/target/target-dispatch-source-test.js
+++ test/spec/iml/target/target-dispatch-source-test.js
@@ -1,56 +1,43 @@
 import highland from 'highland';
-
-import { mock, resetAll } from '../../../system-mock.js';
-
 describe('target dispatch source', () => {
-  let store, stream, socketStream;
-
-  beforeEachAsync(async function() {
-    const CACHE_INITIAL_DATA = {
-      target: ['targets']
-    };
-
+  let mockStore, stream, mockSocketStream;
+  beforeEach(() => {
+    jest.resetModules();
+    const mockCacheInitialData = { target: ['targets'] };
     stream = highland();
+    mockSocketStream = jest.fn(() => stream);
+    mockStore = { dispatch: jest.fn() };
+    jest.mock('../../../../source/iml/store/get-store.js', () => mockStore);
+    jest.mock('../../../../source/iml/environment.js', () => ({
+      CACHE_INITIAL_DATA: mockCacheInitialData,
+      ALLOW_ANONYMOUS_READ: true
+    }));
+    jest.mock(
+      '../../../../source/iml/socket/socket-stream.js',
+      () => mockSocketStream
+    );
 
-    socketStream = jasmine.createSpy('highland').and.returnValue(stream);
-
-    store = {
-      dispatch: jasmine.createSpy('dispatch')
-    };
-
-    await mock('source/iml/target/target-dispatch-source.js', {
-      'source/iml/store/get-store.js': { default: store },
-      'source/iml/environment.js': {
-        CACHE_INITIAL_DATA,
-        ALLOW_ANONYMOUS_READ: true
-      },
-      'source/iml/socket/socket-stream.js': { default: socketStream }
-    });
+    require('../../../../source/iml/target/target-dispatch-source.js');
   });
 
-  afterEach(resetAll);
+  afterEach(() => {
+    window.angular = null;
+  });
 
   it('should dispatch cached targets into the store', () => {
-    expect(store.dispatch).toHaveBeenCalledOnceWith({
+    expect(mockStore.dispatch).toHaveBeenCalledOnceWith({
       type: 'ADD_TARGET_ITEMS',
       payload: ['targets']
     });
   });
-
   it('should setup a persistent socket to /targets', () => {
-    expect(socketStream).toHaveBeenCalledOnceWith('/target', {
-      qs: {
-        limit: 0
-      }
+    expect(mockSocketStream).toHaveBeenCalledOnceWith('/target', {
+      qs: { limit: 0 }
     });
   });
-
   it('should update targets when new items arrive from a persistent socket', () => {
-    stream.write({
-      objects: ['more targets']
-    });
-
-    expect(store.dispatch).toHaveBeenCalledOnceWith({
+    stream.write({ objects: ['more targets'] });
+    expect(mockStore.dispatch).toHaveBeenCalledOnceWith({
       type: 'ADD_TARGET_ITEMS',
       payload: ['more targets']
     });
