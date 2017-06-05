663a482d7f580b6daf7b0da622255afe3c64de36
Will Johnson <william.c.johnson@intel.com>
2017-06-05 16:09:14 -0400

    - fixed get-test-host-stream test
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- test/spec/iml/server/get-test-host-stream-test.js
+++ test/spec/iml/server/get-test-host-stream-test.js
@@ -1,86 +1,57 @@
-import serverModule from '../../../../source/iml/server/server-module';
+import getTestHostStream
+  from '../../../../source/iml/server/get-test-host-stream.js';
 import highland from 'highland';
 
 describe('get test host stream', () => {
-  beforeEach(module(serverModule));
+  let testHostStream, spring, stream, data;
+  beforeEach(() => {
+    stream = highland();
+    spring = jest.fn(() => stream);
+    data = [
+      {
+        address: 'lotus-34vm5.iml.intel.com',
+        status: [
+          { name: 'auth', value: true },
+          { name: 'reverse_ping', value: false }
+        ]
+      }
+    ];
+    testHostStream = getTestHostStream()(spring, {
+      objects: [{ address: 'address1' }]
+    });
+  });
 
-  let getTestHostStream, testHostStream, spring, stream, data;
-
-  beforeEach(
-    inject(function(λ, _getTestHostStream_) {
-      stream = λ();
-      spring = jasmine.createSpy('spring').and.returnValue(stream);
-
-      data = [
-        {
-          address: 'lotus-34vm5.iml.intel.com',
-          status: [
-            {
-              name: 'auth',
-              value: true
-            },
-            {
-              name: 'reverse_ping',
-              value: false
-            }
-          ]
-        }
-      ];
-
-      getTestHostStream = _getTestHostStream_;
-      testHostStream = getTestHostStream(spring, {
-        objects: [{ address: 'address1' }]
-      });
-    })
-  );
-
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(getTestHostStream).toEqual(expect.any(Function));
   });
-
-  it('should return a stream', function() {
+  it('should return a stream', () => {
     const proto = Object.getPrototypeOf(highland());
-
     expect(Object.getPrototypeOf(testHostStream)).toBe(proto);
   });
-
-  it('should post to test_host', function() {
+  it('should post to test_host', () => {
     expect(spring).toHaveBeenCalledOnceWith('testHost', '/test_host', {
       method: 'post',
-      json: {
-        objects: [{ address: 'address1' }]
-      }
+      json: { objects: [{ address: 'address1' }] }
     });
   });
-
-  describe('invoking the pipe', function() {
-    beforeEach(function() {
+  describe('invoking the pipe', () => {
+    beforeEach(() => {
       stream.write(data);
     });
-
-    it('should indicate that the response is invalid', function() {
-      testHostStream.each(function(resp) {
+    it('should indicate that the response is invalid', () => {
+      testHostStream.each(resp => {
         expect(resp.valid).toEqual(false);
       });
     });
-
-    it('should have an updated response value', function() {
-      testHostStream.each(function(resp) {
+    it('should have an updated response value', () => {
+      testHostStream.each(resp => {
         expect(resp).toEqual({
           objects: [
             {
               address: 'lotus-34vm5.iml.intel.com',
               status: [
-                {
-                  name: 'auth',
-                  value: true,
-                  uiName: 'Auth'
-                },
-                {
-                  name: 'reverse_ping',
-                  value: false,
-                  uiName: 'Reverse ping'
-                }
+                { name: 'auth', value: true, uiName: 'Auth' },
+                { name: 'reverse_ping', value: false, uiName: 'Reverse ping' }
               ]
             }
           ],
