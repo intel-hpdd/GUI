13c7635018082a40dd0afe5863ce7b467fddd720
Will Johnson <william.c.johnson@intel.com>
2017-06-05 15:59:37 -0400

    - fix selected servers service test
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- test/spec/iml/server/selected-servers-service-test.js
+++ test/spec/iml/server/selected-servers-service-test.js
@@ -1,55 +1,35 @@
-import serverModule from '../../../../source/iml/server/server-module';
+import selectedServersService
+  from '../../../../source/iml/server/selected-servers-service.js';
 
-describe('selected servers service', function() {
-  beforeEach(module(serverModule));
-
+describe('selected servers service', () => {
   let selectedServers;
+  beforeEach(() => {
+    selectedServers = {};
+    selectedServersService.bind(selectedServers)();
+    selectedServers.servers['https://hostname1.localdomain'] = true;
+  });
 
-  beforeEach(
-    inject(function(_selectedServers_) {
-      selectedServers = _selectedServers_;
-
-      selectedServers.servers['https://hostname1.localdomain'] = true;
-    })
-  );
-
-  it('should be an object', function() {
+  it('should be an object', () => {
     expect(selectedServers).toEqual(expect.any(Object));
   });
-
-  it('should have a servers property', function() {
+  it('should have a servers property', () => {
     expect(selectedServers.servers).toEqual(expect.any(Object));
   });
-
   const dataProvider = [
-    {
-      name: 'all',
-      expected: true
-    },
-
-    {
-      name: 'none',
-      expected: false
-    },
-    {
-      name: 'invert',
-      expected: false
-    }
+    { name: 'all', expected: true },
+    { name: 'none', expected: false },
+    { name: 'invert', expected: false }
   ];
-
   dataProvider.forEach(function checktype(item) {
-    it('should toggle for ' + item.name, function() {
+    it('should toggle for ' + item.name, () => {
       selectedServers.toggleType(item.name);
-
       expect(selectedServers.servers['https://hostname1.localdomain']).toBe(
         item.expected
       );
     });
   });
-
-  it('should add a new server', function() {
+  it('should add a new server', () => {
     selectedServers.addNewServers([{ fqdn: 'https://hostname2.localdomain' }]);
-
     expect(selectedServers.servers['https://hostname2.localdomain']).toBe(
       false
     );
