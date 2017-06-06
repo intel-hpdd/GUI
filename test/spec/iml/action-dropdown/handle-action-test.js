c4af91a9067be0cb64f6c0f8cdb5ec0999bd0772
Will Johnson <william.c.johnson@intel.com>
2017-06-06 12:13:23 -0400

    - fix handle-action test
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- test/spec/iml/action-dropdown/handle-action-test.js
+++ test/spec/iml/action-dropdown/handle-action-test.js
@@ -1,45 +1,35 @@
 import highland from 'highland';
-
-import { mock, resetAll } from '../../../system-mock.js';
-
 describe('handle action', () => {
-  let socketStream,
+  let mockSocketStream,
     actionStream,
     handleAction,
     openConfirmActionModal,
     openResult;
-
-  beforeEachAsync(async function() {
-    jasmine.clock().install();
-
-    socketStream = jasmine.createSpy('socketStream').and.callFake(function() {
+  beforeEach(() => {
+    jest.resetModules();
+    jest.useFakeTimers();
+    mockSocketStream = jest.fn(() => {
       return (actionStream = highland());
     });
-
     openResult = { resultStream: highland() };
-    openConfirmActionModal = jasmine
-      .createSpy('openConfirmActionModal')
-      .and.returnValue(openResult);
+    openConfirmActionModal = jest.fn().mockReturnValue(openResult);
+    jest.mock(
+      '../../../../source/iml/socket/socket-stream.js',
+      () => mockSocketStream
+    );
 
-    const mod = await mock('source/iml/action-dropdown/handle-action.js', {
-      'source/iml/socket/socket-stream.js': { default: socketStream }
-    });
-
+    mod = require('../../../../source/iml/action-dropdown/handle-action.js');
     handleAction = mod.default(openConfirmActionModal);
   });
-
   afterEach(() => {
-    jasmine.clock().uninstall();
+    jest.clearAllTimers();
+    jest.useRealTimers();
+    window.angular = null;
   });
-
-  afterEach(resetAll);
-
-  describe('job', function() {
+  describe('job', () => {
     let record, action;
-
-    beforeEach(function() {
+    beforeEach(() => {
       record = { label: 'foo bar' };
-
       action = {
         verb: 'foo',
         class_name: 'bar',
@@ -47,173 +37,115 @@ describe('handle action', () => {
         confirmation: 'Are you sure you want to foo bar?'
       };
     });
-
-    it('should open the confirm modal if there is confirmation', function() {
+    it('should open the confirm modal if there is confirmation', () => {
       handleAction(record, action);
-
       expect(openConfirmActionModal).toHaveBeenCalledOnceWith('foo(foo bar)', [
         'Are you sure you want to foo bar?'
       ]);
     });
-
-    it('should not open the confirm modal without confirmation', function() {
+    it('should not open the confirm modal without confirmation', () => {
       delete action.confirmation;
-
       handleAction(record, action);
-
       expect(openConfirmActionModal).not.toHaveBeenCalled();
     });
-
-    it('should send the job after confirmation', function() {
-      handleAction(record, action).each(function() {
-        expect(socketStream).toHaveBeenCalledOnceWith(
+    it('should send the job after confirmation', () => {
+      handleAction(record, action).each(() => {
+        expect(mockSocketStream).toHaveBeenCalledOnceWith(
           '/command',
           {
             method: 'post',
             json: {
-              jobs: [
-                {
-                  class_name: 'bar',
-                  args: { some: 'stuff' }
-                }
-              ],
+              jobs: [{ class_name: 'bar', args: { some: 'stuff' } }],
               message: 'foo(foo bar)'
             }
           },
           true
         );
       });
-
       openResult.resultStream.write(true);
-
       actionStream.write({});
     });
-
-    it('should skip the action result', function() {
+    it('should skip the action result', () => {
       handleAction(record, action).each(function(x) {
         expect(x).toBeUndefined();
       }, true);
-
       openResult.resultStream.write(true);
       actionStream.write({ foo: 'bar' });
     });
-
-    it('should not skip the action result', function() {
+    it('should not skip the action result', () => {
       handleAction(record, action).each(function(x) {
         expect(x).toEqual({ foo: 'bar' });
       }, true);
-
       openResult.resultStream.write(false);
       actionStream.write({ foo: 'bar' });
     });
   });
-
-  it('should put the new param for conf param', function() {
+  it('should put the new param for conf param', () => {
     const action = {
       param_key: 'some',
       param_value: 'value',
-      mdt: {
-        resource: 'target',
-        id: '1',
-        conf_params: {}
-      }
+      mdt: { resource: 'target', id: '1', conf_params: {} }
     };
-
     handleAction({}, action);
-
-    expect(socketStream).toHaveBeenCalledOnceWith(
+    expect(mockSocketStream).toHaveBeenCalledOnceWith(
       '/target/1',
       {
         method: 'put',
-        json: {
-          resource: 'target',
-          id: '1',
-          conf_params: {
-            some: 'value'
-          }
-        }
+        json: { resource: 'target', id: '1', conf_params: { some: 'value' } }
       },
       true
     );
   });
-
-  describe('state change', function() {
+  describe('state change', () => {
     let record, action, stream;
-
-    beforeEach(function() {
+    beforeEach(() => {
       record = { resource_uri: '/api/target/2' };
-
       action = { state: 'stopped' };
-
       stream = handleAction(record, action);
     });
-
-    it('should perform a dry run', function() {
-      expect(socketStream).toHaveBeenCalledOnceWith(
+    it('should perform a dry run', () => {
+      expect(mockSocketStream).toHaveBeenCalledOnceWith(
         record.resource_uri,
-        {
-          method: 'put',
-          json: {
-            dry_run: true,
-            state: 'stopped'
-          }
-        },
+        { method: 'put', json: { dry_run: true, state: 'stopped' } },
         true
       );
     });
-
-    describe('dry run', function() {
+    describe('dry run', () => {
       let response;
-
-      beforeEach(function() {
+      beforeEach(() => {
         response = {
-          transition_job: {
-            description: "It's gonna do stuff!"
-          },
+          transition_job: { description: "It's gonna do stuff!" },
           dependency_jobs: [
-            {
-              requires_confirmation: true,
-              description: 'This will do stuff'
-            }
+            { requires_confirmation: true, description: 'This will do stuff' }
           ]
         };
-
         actionStream.write(response);
       });
-
-      it('should open the confirm action modal', function() {
-        stream.each(function() {
+      it('should open the confirm action modal', () => {
+        stream.each(() => {
           expect(
             openConfirmActionModal
           ).toHaveBeenCalledOnceWith("It's gonna do stuff!", [
             'This will do stuff'
           ]);
         });
-
         openResult.resultStream.write(true);
-
         actionStream.write({});
-        jasmine.clock().tick();
+        jest.runAllTimers();
       });
-
-      it('should send the new state after confirm', function() {
-        stream.each(function() {
+      it('should send the new state after confirm', () => {
+        stream.each(() => {
           expect(
-            expect(socketStream).toHaveBeenCalledOnceWith(
+            expect(mockSocketStream).toHaveBeenCalledOnceWith(
               '/api/target/2',
-              {
-                method: 'put',
-                json: { state: 'stopped' }
-              },
+              { method: 'put', json: { state: 'stopped' } },
               true
             )
           );
         });
-
         openResult.resultStream.write(true);
-
         actionStream.write({});
-        jasmine.clock().tick();
+        jest.runAllTimers();
       });
     });
   });
