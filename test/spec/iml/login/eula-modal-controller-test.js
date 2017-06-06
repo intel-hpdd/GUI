f56e662b2017ba92835c46b0330c39e5c26dd1c1
Will Johnson <william.c.johnson@intel.com>
2017-06-06 10:11:30 -0400

    - fix eula-modal-controller test
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- test/spec/iml/login/eula-modal-controller-test.js
+++ test/spec/iml/login/eula-modal-controller-test.js
@@ -1,57 +1,42 @@
-import loginModule from '../../../../source/iml/login/login-module';
-import interceptorsModule
-  from '../../../../source/iml/interceptors/interceptor-module';
+import eulaModalController
+  from '../../../../source/iml/login/eula-modal-controller.js';
+import angular from '../../../angular-mock-setup.js';
 
 describe('Eula Modal Controller', () => {
-  let $scope, $httpBackend, $uibModalInstance;
-
+  let $scope, $httpBackend, $uibModalInstance, help, userModel;
   beforeEach(
-    module(loginModule, interceptorsModule, $provide => {
-      $provide.value('help', {
-        get: jasmine.createSpy('get').and.returnValue('foo')
-      });
-    })
-  );
-
-  beforeEach(
-    inject(($controller, $rootScope, _$httpBackend_, UserModel) => {
+    angular.mock.inject(($rootScope, _$httpBackend_, $http, $q) => {
       $scope = $rootScope.$new();
       $httpBackend = _$httpBackend_;
-
-      $uibModalInstance = {
-        close: jasmine.createSpy('close'),
-        dismiss: jasmine.createSpy('dismiss')
+      help = {
+        get: jest.fn(() => 'foo')
       };
-
-      $controller('EulaCtrl', {
-        $scope,
-        $uibModalInstance,
-        user: new UserModel()
-      });
+      $uibModalInstance = { close: jest.fn(), dismiss: jest.fn() };
+      userModel = {
+        actOnEulaState: jest.fn(),
+        $update: jest.fn(() => {
+          $http.put('user/', { accepted_eula: userModel.accepted_eula });
+          return $q.resolve('action-arg');
+        })
+      };
+      eulaModalController($scope, $uibModalInstance, help, userModel);
     })
   );
-
   it('should update the user on accept', () => {
     $httpBackend.expectPUT('user/', { accepted_eula: true }).respond(202);
-
     $scope.eulaCtrl.accept();
-
     expect($uibModalInstance.close).not.toHaveBeenCalled();
-
     $httpBackend.flush();
-
-    expect($uibModalInstance.close).toHaveBeenCalled();
+    expect($uibModalInstance.close).toHaveBeenCalledWith('close', 'action-arg');
   });
-
   it('should perform the appropriate actions on reject', () => {
     $httpBackend.expectPUT('user/', { accepted_eula: false }).respond(202);
-
     expect($uibModalInstance.dismiss).not.toHaveBeenCalled();
-
     $scope.eulaCtrl.reject();
-
     $httpBackend.flush();
-
-    expect($uibModalInstance.dismiss).toHaveBeenCalled();
+    expect($uibModalInstance.dismiss).toHaveBeenCalledWith(
+      'dismiss',
+      'action-arg'
+    );
   });
 });
