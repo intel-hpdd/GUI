a515a9826a68ffd467f5db315235f3cacfa8cbbe
Will Johnson <william.c.johnson@intel.com>
2017-06-13 19:08:43 -0400

    - fixed get-heat-map-legend test
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- test/dom/iml/heat-map/get-heat-map-legend-test.js
+++ test/dom/iml/heat-map/get-heat-map-legend-test.js
@@ -1,26 +1,19 @@
 import _ from '@mfl/lodash-mixins';
+import getHeatMapLegend
+  from '../../../../source/iml/heat-map/get-heat-map-legend.js';
+import d3 from 'd3';
 
-describe('the heat map legend', function() {
-  beforeEach(module('heatMap'));
-
-  let getHeatMapLegend, heatMapLegend;
-
-  beforeEach(
-    inject(function(_getHeatMapLegend_) {
-      getHeatMapLegend = _getHeatMapLegend_;
-
-      heatMapLegend = getHeatMapLegend();
-    })
-  );
-
-  it('should be callable', function() {
+describe('the heat map legend', () => {
+  let heatMapLegend;
+  beforeEach(() => {
+    heatMapLegend = getHeatMapLegend()();
+  });
+  it('should be callable', () => {
     expect(heatMapLegend).toEqual(expect.any(Function));
   });
-
-  it('should set destroy to a noop', function() {
-    expect(heatMapLegend.destroy).toBe(_.noop);
+  it('should set destroy to a noop', () => {
+    expect(heatMapLegend.destroy).toEqual(jasmine.any(Function));
   });
-
   const accessors = [
     'colorScale',
     'legendScale',
@@ -30,83 +23,64 @@ describe('the heat map legend', function() {
     'height',
     'margin'
   ];
-
-  accessors.forEach(function(accessor) {
-    it('should have a ' + accessor + 'accessor', function() {
+  accessors.forEach(accessor => {
+    it('should have a ' + accessor + 'accessor', () => {
       expect(heatMapLegend[accessor]).toEqual(expect.any(Function));
     });
-
-    it('should set ' + accessor, function() {
+    it('should set ' + accessor, () => {
       const val = { foo: 'bar' };
-
       heatMapLegend[accessor](val);
-
       expect(heatMapLegend[accessor]()).toBe(val);
     });
   });
+  describe('when populated', () => {
+    let svg, heatMapLegendGroup, selection, setup, query, queryAll;
+    beforeEach(() => {
+      HTMLElement.prototype.getBBox = () => ({
+        height: 100,
+        width: 100,
+        x: 0,
+        y: 0
+      });
 
-  describe('when populated', function() {
-    let d3, svg, heatMapLegendGroup, selection, setup, query, queryAll;
-
-    beforeEach(
-      inject(function(_d3_) {
-        d3 = _d3_;
-
-        heatMapLegendGroup = document.createElementNS(
-          'http://www.w3.org/2000/svg',
-          'g'
-        );
-        selection = d3.select(heatMapLegendGroup);
-
-        svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-
-        document.body.appendChild(svg);
-
-        svg.appendChild(heatMapLegendGroup);
-
-        query = heatMapLegendGroup.querySelector.bind(heatMapLegendGroup);
-        queryAll = heatMapLegendGroup.querySelectorAll.bind(heatMapLegendGroup);
-
-        const colors = ['#8ebad9', '#d6e2f3', '#fbb4b4', '#fb8181', '#ff6262'];
-        const color = d3.scale
-          .linear()
-          .range(colors)
-          .domain(d3.range(0, 1, 1.0 / (colors.length - 1)).concat(1));
-
-        const legend = d3.scale.linear().domain([0, 99]).range([0, 1]);
-
-        heatMapLegend
-          .width(500)
-          .height(30)
-          .margin({
-            top: 5,
-            right: 0,
-            bottom: 5,
-            left: 0
-          })
-          .colorScale(color)
-          .legendScale(legend)
-          .zScale(d3.scale.linear().range([0, 1]))
-          .formatter(_.identity);
-
-        const zValue = _.pluckPath('data.stats_read_bytes');
-
-        setup = function setup(d) {
-          const merged = _.flatten(d);
-
-          heatMapLegend.zScale().domain(d3.extent(merged, zValue));
-
-          selection.datum(d).call(heatMapLegend);
-        };
-      })
-    );
-
-    afterEach(function() {
-      document.body.removeChild(svg);
+      heatMapLegendGroup = document.createElementNS(
+        'http://www.w3.org/2000/svg',
+        'g'
+      );
+      selection = d3.select(heatMapLegendGroup);
+      svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+      document.body.appendChild(svg);
+      svg.appendChild(heatMapLegendGroup);
+      query = heatMapLegendGroup.querySelector.bind(heatMapLegendGroup);
+      queryAll = heatMapLegendGroup.querySelectorAll.bind(heatMapLegendGroup);
+      const colors = ['#8ebad9', '#d6e2f3', '#fbb4b4', '#fb8181', '#ff6262'];
+      const color = d3.scale
+        .linear()
+        .range(colors)
+        .domain(d3.range(0, 1, 1.0 / (colors.length - 1)).concat(1));
+      const legend = d3.scale.linear().domain([0, 99]).range([0, 1]);
+      heatMapLegend
+        .width(500)
+        .height(30)
+        .margin({ top: 5, right: 0, bottom: 5, left: 0 })
+        .colorScale(color)
+        .legendScale(legend)
+        .zScale(d3.scale.linear().range([0, 1]))
+        .formatter(_.identity);
+      const zValue = _.pluckPath('data.stats_read_bytes');
+      setup = function setup(d) {
+        const merged = _.flatten(d);
+        heatMapLegend.zScale().domain(d3.extent(merged, zValue));
+        selection.datum(d).call(heatMapLegend);
+      };
     });
 
-    describe('with multiple data points', function() {
-      beforeEach(function() {
+    afterEach(() => {
+      document.body.removeChild(svg);
+      delete HTMLElement.prototype.getBBox;
+    });
+    describe('with multiple data points', () => {
+      beforeEach(() => {
         setup([
           [
             {
@@ -122,16 +96,13 @@ describe('the heat map legend', function() {
           ]
         ]);
       });
-
-      it('should have 100 steps', function() {
+      it('should have 100 steps', () => {
         expect(queryAll('.step').length).toBe(100);
       });
-
-      it('should set the min text', function() {
+      it('should set the min text', () => {
         expect(Math.floor(query('.min').innerHTML)).toEqual(8091667852);
       });
-
-      it('should set the max text', function() {
+      it('should set the max text', () => {
         expect(Math.floor(query('.max').innerHTML)).toEqual(9357931867);
       });
     });
