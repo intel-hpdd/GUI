7e38350305380ec75eb9d8f9389d5699232e1ef7
Will Johnson <william.c.johnson@intel.com>
2017-06-14 15:46:06 -0400

    - fixed pacemaker-state test
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- test/dom/iml/pacemaker/pacemaker-state-test.js
+++ test/dom/iml/pacemaker/pacemaker-state-test.js
@@ -1,19 +1,23 @@
 import highland from 'highland';
-import pacemakerModule from '../../../../source/iml/pacemaker/pacemaker-module';
+import pacemakerState
+  from '../../../../source/iml/pacemaker/pacemaker-state.js';
+import angular from '../../../angular-mock-setup.js';
 
 describe('pacemaker state directive', () => {
-  beforeEach(module(pacemakerModule));
-
   let el, $scope;
 
   beforeEach(
-    inject(($rootScope, $compile) => {
-      const template = '<pacemaker-state stream="stream"></pacemaker-state>';
+    angular.mock.module($compileProvider => {
+      $compileProvider.component('pacemakerState', pacemakerState);
+    })
+  );
 
+  beforeEach(
+    angular.mock.inject(($rootScope, $compile) => {
+      const template = '<pacemaker-state stream="stream"></pacemaker-state>';
       $scope = $rootScope.$new();
       $scope.stream = highland();
-      spyOn($scope.stream, 'destroy');
-
+      jest.spyOn($scope.stream, 'destroy');
       el = $compile(template)($scope)[0];
       $scope.$digest();
     })
@@ -26,30 +30,24 @@ describe('pacemaker state directive', () => {
   ];
 
   states.forEach(state => {
-    it(`should display state for ${state[0]} with no host id`, () => {
-      $scope.stream.write({
-        state: state[1]
-      });
-
+    it(`should display state for ${state[0]} with no host  id`, () => {
+      $scope.stream.write({ state: state[1] });
       expect(el.querySelector('span').textContent.trim()).toEqual(state[0]);
     });
   });
 
   it('should display nothing when there is no data', () => {
     $scope.stream.write();
-
     expect(el.querySelector('span')).toBeNull();
   });
 
   it('should display nothing when there is bad data', () => {
     $scope.stream.write(null);
-
     expect(el.querySelector('span')).toBeNull();
   });
 
   it('should destroy the stream when the scope is destroyed', () => {
     $scope.$destroy();
-
     expect($scope.stream.destroy).toHaveBeenCalledTimes(1);
   });
 });
