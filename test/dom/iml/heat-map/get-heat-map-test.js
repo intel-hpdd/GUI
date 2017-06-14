b85cd84fab1b43283b1b1dc279cbc9d0e636207a
Will Johnson <william.c.johnson@intel.com>
2017-06-14 17:25:44 -0400

    - fixed get-heat-map test
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- test/dom/iml/heat-map/get-heat-map-test.js
+++ test/dom/iml/heat-map/get-heat-map-test.js
@@ -1,19 +1,13 @@
 import _ from '@mfl/lodash-mixins';
-import heatMapModule from '../../../../source/iml/heat-map/heat-map-module.js';
-import angular from '../../../angular-mock-setup.js';
+import d3 from 'd3';
+import getHeatMap from '../../../../source/iml/heat-map/get-heat-map.js';
 
 describe('the heat map', () => {
-  beforeEach(angular.mock.module(heatMapModule));
+  let heatMap;
 
-  let getHeatMap, heatMap;
-
-  beforeEach(
-    inject(_getHeatMap_ => {
-      getHeatMap = _getHeatMap_;
-
-      heatMap = getHeatMap();
-    })
-  );
+  beforeEach(() => {
+    heatMap = getHeatMap()();
+  });
 
   it('should be callable', () => {
     expect(heatMap).toEqual(expect.any(Function));
@@ -34,7 +28,6 @@ describe('the heat map', () => {
     'height',
     'duration'
   ];
-
   accessors.forEach(accessor => {
     it('should have a ' + accessor + 'accessor', () => {
       expect(heatMap[accessor]).toEqual(expect.any(Function));
@@ -42,70 +35,60 @@ describe('the heat map', () => {
 
     it('should set ' + accessor, () => {
       const val = { foo: 'bar' };
-
       heatMap[accessor](val);
-
       expect(heatMap[accessor]()).toBe(val);
     });
   });
 
   describe('when populated', () => {
-    let d3, heatMapGroup, selection, setup, query, queryAll;
+    let heatMapGroup, selection, setup, query, queryAll;
 
-    beforeEach(
-      inject(_d3_ => {
-        d3 = _d3_;
+    beforeEach(() => {
+      heatMapGroup = document.createElement('g');
+      selection = d3.select(heatMapGroup);
 
-        heatMapGroup = document.createElement('g');
-        selection = d3.select(heatMapGroup);
+      const svg = document.createElement('svg');
+      svg.appendChild(heatMapGroup);
+      query = heatMapGroup.querySelector.bind(heatMapGroup);
+      queryAll = heatMapGroup.querySelectorAll.bind(heatMapGroup);
 
-        const svg = document.createElement('svg');
-        svg.appendChild(heatMapGroup);
+      const colors = ['#8ebad9', '#d6e2f3', '#fbb4b4', '#fb8181', '#ff6262'];
 
-        query = heatMapGroup.querySelector.bind(heatMapGroup);
-        queryAll = heatMapGroup.querySelectorAll.bind(heatMapGroup);
+      const color = d3.scale
+        .linear()
+        .range(colors)
+        .domain(d3.range(0, 1, 1.0 / (colors.length - 1)).concat(1));
 
-        const colors = ['#8ebad9', '#d6e2f3', '#fbb4b4', '#fb8181', '#ff6262'];
-        const color = d3.scale
-          .linear()
-          .range(colors)
-          .domain(d3.range(0, 1, 1.0 / (colors.length - 1)).concat(1));
+      const zValue = _.pluckPath('data.stats_read_bytes');
 
-        const zValue = _.pluckPath('data.stats_read_bytes');
+      heatMap
+        .width(500)
+        .height(500)
+        .xScale(d3.time.scale())
+        .xValue(xValue)
+        .yScale(d3.scale.ordinal())
+        .yValue(_.property('id'))
+        .zScale(d3.scale.linear().range([0, 1]))
+        .zValue(zValue)
+        .colorScale(color);
 
-        heatMap
-          .width(500)
-          .height(500)
-          .xScale(d3.time.scale())
-          .xValue(xValue)
-          .yScale(d3.scale.ordinal())
-          .yValue(_.property('id'))
-          .zScale(d3.scale.linear().range([0, 1]))
-          .zValue(zValue)
-          .colorScale(color);
+      function xValue(d) {
+        return new Date(d.ts);
+      }
 
-        function xValue(d) {
-          return new Date(d.ts);
-        }
+      setup = d => {
+        const merged = _.flatten(d);
+        const keys = _(merged).pluck('id').uniq().value();
 
-        setup = d => {
-          const merged = _.flatten(d);
-          const keys = _(merged).pluck('id').uniq().value();
-
-          heatMap.xScale().domain(d3.extent(merged, xValue)).range([0, 500]);
-
-          heatMap.yScale().domain(keys).rangePoints([0, 500], 1.0);
-
-          heatMap.zScale().domain(d3.extent(merged, zValue));
-
-          selection.datum(d).call(heatMap);
-        };
-      })
-    );
+        heatMap.xScale().domain(d3.extent(merged, xValue)).range([0, 500]);
+        heatMap.yScale().domain(keys).rangePoints([0, 500], 1.0);
+        heatMap.zScale().domain(d3.extent(merged, zValue));
+        selection.datum(d).call(heatMap);
+      };
+    });
 
     it('should have 0 rows when there is no data', () => {
       selection.datum([]).call(heatMap);
-
       expect(heatMapGroup.querySelector('.row')).toEqual(null);
     });
 
@@ -150,11 +133,9 @@ describe('the heat map', () => {
 
       it('should remove the cell on exit', () => {
         setup([]);
-
         expect(queryAll('.cell').length).toBe(0);
       });
     });
-
     describe('with multiple data points', () => {
       beforeEach(() => {
         setup([
