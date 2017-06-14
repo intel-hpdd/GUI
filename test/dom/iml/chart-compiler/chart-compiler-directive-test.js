8a2bc0e667b2b0578ba987c41bf89776874a2444
Will Johnson <william.c.johnson@intel.com>
2017-06-14 12:13:45 -0400

    - fixed chart-compiler-directive test
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- test/dom/iml/chart-compiler/chart-compiler-directive-test.js
+++ test/dom/iml/chart-compiler/chart-compiler-directive-test.js
@@ -1,36 +1,35 @@
-import chartCompilerModule
-  from '../../../../source/iml/chart-compiler/chart-compiler-module';
+import {
+  chartCompilerDirective
+} from '../../../../source/iml/chart-compiler/chart-compiler-directive.js';
+import angular from '../../../angular-mock-setup.js';
 
 describe('chart compiler directive', () => {
-  beforeEach(module(chartCompilerModule));
+  beforeEach(
+    angular.mock.module($compileProvider => {
+      $compileProvider.directive('chartCompiler', chartCompilerDirective);
+    })
+  );
 
   let $scope, el, spy;
-
   beforeEach(
-    inject(($rootScope, $compile) => {
+    angular.mock.inject(($rootScope, $compile) => {
       const template = '<div chart-compiler chart="chart"></div>';
-
-      spy = jasmine.createSpy('spy');
-
+      spy = jest.fn();
       $scope = $rootScope.$new();
       $scope.chart = {
         template: '<div class="tha-chart">I\'m a chart!</div>',
         stream: 'stream',
-        chartFn: spy.and.returnValue()
+        chartFn: spy.mockReturnValue()
       };
-
       el = $compile(template)($scope)[0];
       $scope.$apply();
     })
   );
-
   it('should add the chart', () => {
     expect(el.querySelector('.tha-chart')).not.toBeNull();
   });
-
   it('should call the chart with scope and stream', () => {
     const $scopeConstructor = Object.getPrototypeOf($scope).constructor;
-
     expect(spy).toHaveBeenCalledOnceWith(
       expect.any($scopeConstructor),
       'stream'
