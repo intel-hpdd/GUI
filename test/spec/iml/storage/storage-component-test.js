4d68f6b4acd3ffa965fe22054d2703d6eb243a00
Peter Robinson <peter.timothy.robinson@gmail.com>
2017-06-16 09:17:27 -0600

    Add functionality for a basic storage plugin
     - Added angular to inferno integration
     - Added basic test case.

--- test/spec/iml/storage/storage-component-test.js
+++ test/spec/iml/storage/storage-component-test.js
@@ -1,10 +1,22 @@
 import storageComponent
   from '../../../../source/iml/storage/storage-component.js';
+import Inferno from 'inferno';
+import { querySelector } from '../../../../source/iml/dom-utils.js';
 
 describe('storage component', () => {
+  let root;
+  beforeEach(() => {
+    root = document.createElement('root');
+    Inferno.render(<storageComponent />, root);
+  });
+
   it('should call the controller function', () => {
     spyOn(storageComponent, 'controller');
     storageComponent.controller();
     expect(storageComponent.controller).toHaveBeenCalled();
+  });
+
+  it.only('should have the no-plugins class', () => {
+    expect(querySelector(root, '.no-plugins')).not.toBeNull();
   });
 });
