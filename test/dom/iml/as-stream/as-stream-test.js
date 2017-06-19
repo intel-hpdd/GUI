b75aec310fde3b9351c49d4fe4c00d11cfba0b71
Will Johnson <william.c.johnson@intel.com>
2017-06-19 12:50:46 -0400

    - fixed as stream test
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- test/dom/iml/as-stream/as-stream-test.js
+++ test/dom/iml/as-stream/as-stream-test.js
@@ -1,21 +1,25 @@
 import * as fp from '@mfl/fp';
 import highland from 'highland';
 import angular from '../../../angular-mock-setup.js';
-import asStreamModule from '../../../../source/iml/as-stream/as-stream-module';
-import asValueModule from '../../../../source/iml/as-value/as-value-module';
+import asValue from '../../../../source/iml/as-value/as-value.js';
 
 describe('As stream', () => {
-  let s;
+  let s, mockHighland, asStream;
 
   beforeEach(
-    angular.mock.module(asStreamModule, asValueModule, function($provide) {
-      $provide.value('highland', function() {
-        s = highland();
+    angular.mock.module($compileProvider => {
+      s = highland();
+      jest.spyOn(s, 'destroy');
 
-        spyOn(s, 'destroy');
+      mockHighland = jest.fn(() => s);
 
-        return s;
-      });
+      jest.mock('highland', () => mockHighland);
+
+      asStream = require('../../../../source/iml/as-stream/as-stream.js')
+        .default;
+
+      $compileProvider.directive('asStream', asStream);
+      $compileProvider.directive('asValue', asValue);
     })
   );
 
@@ -70,7 +74,6 @@ describe('As stream', () => {
 
   it('should destroy the stream on scope destruction', function() {
     $scope.$destroy();
-
     expect(s.destroy).toHaveBeenCalledTimes(1);
   });
 });
