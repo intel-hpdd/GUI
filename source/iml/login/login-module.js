5703ea233f516a3ac24150d24297806348612e14
Joe Grund <joe.grund@intel.com>
2017-08-11 12:15:45 -0400

    Remove scroll-boundary from EULA (#67)
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- source/iml/login/login-module.js
+++ source/iml/login/login-module.js
@@ -8,7 +8,6 @@
 import angular from 'angular';
 import authModule from '../auth/auth-module';
 import remoteValidateModule from '../remote-validate/remote-validate-module';
-import atScrollBoundaryModule from '../at-scroll-boundary/at-scroll-boundary-module';
 import accessDeniedModule from '../access-denied/access-denied-module';
 import tooltipModule from '../tooltip/tooltip-module';
 import navigateModule from '../navigate/navigate-module';
@@ -19,7 +18,6 @@ export default angular
   .module('login', [
     authModule,
     remoteValidateModule,
-    atScrollBoundaryModule,
     accessDeniedModule,
     tooltipModule,
     navigateModule
