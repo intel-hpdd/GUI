e5b3587c886f9b5f1a3bdeb7a98b301a2543f767
Will Johnson <william.c1.johnson@gmail.com>
2018-06-20 18:36:39 -0400

    Fixes #113. Show tooltip on disabled upgrade button.
    Signed-off-by: Will Johnson <william.c1.johnson@gmail.com>

--- source/iml/server/server-states.js
+++ source/iml/server/server-states.js
@@ -175,8 +175,8 @@ export const serverState = {
     <h4 class="section-header">Server Actions</h4>
 
     <div class="action-buttons">
-      <span ng-repeat="action in server.actions track by action.value">
-        <a type="button" class="btn btn-primary btn-sm tooltip-container tooltip-hover"
+      <span class="tooltip-container tooltip-hover" ng-repeat="action in server.actions track by action.value">
+        <a type="button" class="btn btn-primary btn-sm"
            ng-class="{disabled: action.buttonDisabled(server.servers)}"
            ng-click="server.setEditName(action.value)">
           {{action.value}}
