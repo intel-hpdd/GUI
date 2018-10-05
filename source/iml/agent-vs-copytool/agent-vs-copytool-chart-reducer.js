a71c1300114faef3af51dd17bbb6702b67bbb0cd
Will Johnson <wjohnson@whamcloud.com>
2018-10-05 11:11:30 -0400

    Integrate Immer.js and update reducers to use it
    Fixes #146.
    
    Description:
    Integreating immer will guarantee immutability within the store. This is
    crucial for the integration of new items into the store as we need to be
    sure that their data is not modified outside their scope.
    
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

--- source/iml/agent-vs-copytool/agent-vs-copytool-chart-reducer.js
+++ source/iml/agent-vs-copytool/agent-vs-copytool-chart-reducer.js
@@ -8,29 +8,19 @@
 export const UPDATE_AGENT_VS_COPYTOOL_CHART_ITEMS = "UPDATE_AGENT_VS_COPYTOOL_CHART_ITEMS";
 export const DEFAULT_AGENT_VS_COPYTOOL_CHART_ITEMS = "DEFAULT_AGENT_VS_COPYTOOL_CHART_ITEMS";
 
-import type { durationPayloadHashT, durationPayloadT } from "../duration-picker/duration-picker-module.js";
+import produce from "immer";
 
+import type { durationPayloadHashT } from "../duration-picker/duration-picker-module.js";
 import type { addAgentVsCopytoolActionT } from "./agent-vs-copytool-module.js";
 
-function mergeState(state: durationPayloadHashT, payload: durationPayloadT) {
-  return Object.assign({}, state, {
-    [payload.page]: { ...state[payload.page], ...payload }
-  });
-}
-
-export default function(
-  state: durationPayloadHashT = {},
-  { type, payload }: addAgentVsCopytoolActionT
-): durationPayloadHashT {
-  switch (type) {
-    case DEFAULT_AGENT_VS_COPYTOOL_CHART_ITEMS:
-      if (!state[payload.page]) state = mergeState(state, payload);
-
-      return state;
-    case UPDATE_AGENT_VS_COPYTOOL_CHART_ITEMS:
-      return mergeState(state, payload);
-
-    default:
-      return state;
-  }
-}
+export default (state: durationPayloadHashT = {}, { type, payload }: addAgentVsCopytoolActionT): durationPayloadHashT =>
+  produce(state, draft => {
+    switch (type) {
+      case DEFAULT_AGENT_VS_COPYTOOL_CHART_ITEMS:
+        if (!draft[payload.page]) draft[payload.page] = payload;
+        break;
+      case UPDATE_AGENT_VS_COPYTOOL_CHART_ITEMS:
+        draft[payload.page] = { ...state[payload.page], ...payload };
+        break;
+    }
+  }) || {};
