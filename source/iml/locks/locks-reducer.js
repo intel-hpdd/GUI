fcfdb63d87063fdf64a20d2be6debaff67458ce4
Will Johnson <wjohnson@whamcloud.com>
2019-01-22 14:55:56 -0500

    - Add "remove" as an action to locks reducer
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

--- source/iml/locks/locks-reducer.js
+++ source/iml/locks/locks-reducer.js
@@ -10,6 +10,7 @@ import Immutable from "seamless-immutable";
 export const UPDATE_LOCKS_ACTION: "UPDATE_LOCKS" = "UPDATE_LOCKS";
 
 export const LOCK_ACTION_ADD: "add" = "add";
+export const LOCK_ACTION_REMOVE: "remove" = "remove";
 
 export const LOCK_TYPE_READ: "read" = "read";
 export const LOCK_TYPE_WRITE: "write" = "write";
@@ -19,7 +20,7 @@ type ResourceIdT = string;
 type LockKeyT = ContentTypeIdT | ":" | ResourceIdT;
 
 type LockEntryT = {|
-  action: LOCK_ACTION_ADD,
+  action: LOCK_ACTION_ADD | LOCK_ACTION_REMOVE,
   content_type_id: number,
   description: string,
   item_id: number,
