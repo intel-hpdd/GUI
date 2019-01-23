37f0d709e5de8dfcb093ee1b88de6e67154d5222
Will Johnson <wjohnson@whamcloud.com>
2019-01-23 17:43:25 -0500

    - update tests
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

--- test/spec/iml/disconnect-modal/disconnect-modal-test.js
+++ test/spec/iml/disconnect-modal/disconnect-modal-test.js
@@ -1,60 +1,25 @@
-import angular from "../../../angular-mock-setup.js";
-import windowUnloadFactory from "../../../../source/iml/disconnect-modal/disconnect-modal.js";
+// @flow
+
+import { render } from "inferno";
+import global from "../../../../source/iml/global.js";
+import DisconnectModal from "../../../../source/iml/disconnect-modal/disconnect-modal.js";
 
 describe("disconnect modal", () => {
-  let $uibModal, $timeout, modal, $window, disconnectModalFactory, disconnectModal, mockWindowUnload;
-
-  beforeEach(() => {});
-
-  beforeEach(
-    angular.mock.inject((_$timeout_, _$window_) => {
-      $timeout = _$timeout_;
-      $window = _$window_;
-    })
-  );
+  let el;
 
   beforeEach(() => {
-    modal = {};
-    $uibModal = {
-      open: jest.fn(() => modal)
-    };
+    el = document.createElement("div");
+    if (global.document.body) global.document.body.appendChild(el);
 
-    mockWindowUnload = windowUnloadFactory($window);
-    disconnectModalFactory = require("../../../../source/iml/disconnect-modal/disconnect-modal.js").default;
-
-    disconnectModal = disconnectModalFactory(mockWindowUnload, $uibModal, $timeout);
+    render(<DisconnectModal />, el);
   });
 
   afterEach(() => {
-    mockWindowUnload.unloading = false;
+    render(null, el);
+    if (global.document.body) global.document.body.removeChild(el);
   });
 
-  it("should call the modal with the expected params", () => {
-    disconnectModal.open();
-    $timeout.flush();
-
-    expect($uibModal.open).toHaveBeenCalledWith({
-      backdrop: "static",
-      windowClass: "disconnect-modal",
-      keyboard: false,
-      template: expect.any(String)
-    });
-  });
-
-  it("should not open the modal if window has unloaded", () => {
-    mockWindowUnload.unloading = true;
-    disconnectModal.open();
-    $timeout.flush();
-
-    expect($uibModal.open).not.toHaveBeenCalled();
-  });
-
-  it("should not open the modal if the modal already exists", () => {
-    disconnectModal.open();
-    $timeout.flush();
-    disconnectModal.open();
-    $timeout.flush();
-
-    expect($uibModal.open).toHaveBeenCalledTimes(1);
+  it("should generate the modal", () => {
+    expect(el).toMatchSnapshot();
   });
 });
--- test/spec/iml/sse/sse-stream-test.js
+++ test/spec/iml/sse/sse-stream-test.js
@@ -1,18 +1,41 @@
 // @flow
 
+import {
+  SET_CONNECT_SSE,
+  SET_DISCONNECT_SSE
+} from "../../../../source/iml/disconnect-modal/disconnect-modal-reducer.js";
+
 describe("sse stream", () => {
-  let mockSseUrl, mockEventSource, eventSource, getStream, sse$;
+  let mockSseUrl, mockEventSource, mockGetStore, mockBackoff, backoff, eventSource, getStream, sse$;
   beforeEach(() => {
     mockSseUrl = "https://localhost:8443/messaging";
     jest.mock("../../../../source/iml/environment.js", () => ({
       SSE: mockSseUrl
     }));
 
-    eventSource = {};
+    eventSource = {
+      close: jest.fn(),
+      onopen: jest.fn(),
+      onerror: jest.fn(),
+      onmessage: jest.fn()
+    };
     mockEventSource = jest.fn(() => eventSource);
     jest.mock("../../../../source/iml/global.js", () => ({
       EventSource: mockEventSource
     }));
+
+    mockGetStore = {
+      dispatch: jest.fn()
+    };
+    jest.mock("../../../../source/iml/store/get-store.js", () => mockGetStore);
+
+    backoff = {
+      reset: jest.fn(),
+      duration: jest.fn(() => 10)
+    };
+
+    mockBackoff = jest.fn(() => backoff);
+    jest.mock("backo", () => mockBackoff);
 
     getStream = require("../../../../source/iml/sse/sse-stream.js").default;
     sse$ = getStream();
@@ -46,22 +69,85 @@ describe("sse stream", () => {
     });
   });
 
-  it("should handle an error on the event source", done => {
-    sse$
-      .errors(e => {
-        expect(e).toEqual(new Error("An error occurred on the event source."));
-        done();
-      })
-      .each(() => {
+  describe("when an error occurs on the stream", () => {
+    let e;
+    beforeEach(() => {
+      e = {
+        currentTarget: {
+          readyState: 2
+        }
+      };
+    });
+
+    it("should handle the error", done => {
+      sse$
+        .errors(e => {
+          expect(e).toEqual(new Error("An error occurred on the event source."));
+          done();
+        })
+        .each(() => {
+          done.fail();
+        });
+
+      eventSource.onerror(e);
+    });
+
+    it("should set the sse state to disconnected", done => {
+      sse$
+        .errors(() => {
+          expect(mockGetStore.dispatch).toHaveBeenCalledTimes(1);
+          expect(mockGetStore.dispatch).toHaveBeenCalledWith({ type: SET_DISCONNECT_SSE, payload: {} });
+          done();
+        })
+        .each(() => {
+          done.fail();
+        });
+
+      eventSource.onerror(e);
+    });
+
+    it("should retrieve the backoff duration", done => {
+      sse$.errors(() => {}).each(() => {
         done.fail();
       });
 
-    const e = {
-      currentTarget: {
-        readyState: 2
-      }
-    };
+      eventSource.close.mockImplementation(() => {
+        expect(backoff.duration).toHaveBeenCalledTimes(1);
+        done();
+      });
 
-    eventSource.onerror(e);
+      eventSource.onerror(e);
+    });
+
+    it("should close the stream after the duration", done => {
+      sse$.errors(() => {}).each(() => {
+        done.fail();
+      });
+
+      eventSource.close.mockImplementation(() => {
+        expect(eventSource.close).toHaveBeenCalledTimes(1);
+        done();
+      });
+
+      eventSource.onerror(e);
+    });
+  });
+
+  describe("when the stream is opened", () => {
+    beforeEach(() => {
+      sse$.each(() => {});
+
+      eventSource.onopen();
+    });
+
+    it("should set sse to a connected status when the stream is opened", done => {
+      expect(mockGetStore.dispatch).toHaveBeenCalledTimes(1);
+      expect(mockGetStore.dispatch).toHaveBeenCalledWith({ type: SET_CONNECT_SSE, payload: {} });
+      done();
+    });
+
+    it("should reset backoff", () => {
+      expect(backoff.reset).toHaveBeenCalledTimes(1);
+    });
   });
 });
