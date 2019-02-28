480d4289c85842413ddf3ccb4558ed777d4e2e92
Will Johnson <wjohnson@whamcloud.com>
2019-02-28 11:59:31 -0500

    Fix hsm action dropdown button.

--- source/iml/hsm/hsm-fs-controller.js
+++ source/iml/hsm/hsm-fs-controller.js
@@ -1,11 +1,9 @@
 // @flow
 
 //
-// Copyright (c) 2018 DDN. All rights reserved.
+// Copyright (c) 2019 DDN. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
-
-import * as fp from "@iml/fp";
 
 import type { StateServiceT } from "angular-ui-router";
 
@@ -58,10 +56,10 @@ export default function HsmFsCtrl(
       }
     })
     .each(() => {
-      const fsId = $state.router.globals.params.fsId;
+      const fsId = parseInt($state.router.globals.params.fsId, 10);
 
       if (fsId) {
-        fsStream2 = fsStream().map(fp.find(x => x.id === fsId));
+        fsStream2 = fsStream().map(xs => xs.find(x => x.id === fsId));
         p("fs", fsStream2);
       }
     });
