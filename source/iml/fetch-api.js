fdd48caddffcd229be74e8a1c0a0a2262d20f9de
Will Johnson <william.c1.johnson@gmail.com>
2019-03-05 17:16:32 -0500

    Add fetch-api test

--- source/iml/fetch-api.js
+++ source/iml/fetch-api.js
@@ -7,18 +7,17 @@
 
 import getStore from "./store/get-store.js";
 import global from "./global.js";
-//import { SHOW_EXCEPTION_MODAL_ACTION } from "./exception/exception-modal-reducer.js";
+import { SHOW_EXCEPTION_MODAL_ACTION } from "./exception/exception-modal-reducer.js";
 
-console.log("global", global);
 export default function fetchApi<T>(url: string, options: Object = {}): Promise<?T> {
   const r: Promise<?T> = global
     .fetch(url, options)
     .then(x => x.json())
     .catch(e => {
-      // getStore.dispatch({
-      //   type: SHOW_EXCEPTION_MODAL_ACTION,
-      //   payload: e
-      // });
+      getStore.dispatch({
+        type: SHOW_EXCEPTION_MODAL_ACTION,
+        payload: e
+      });
     });
 
   return r;
