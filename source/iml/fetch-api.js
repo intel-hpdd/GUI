363c92ac9333b7155596b0114770649ad5067c0e
Will Johnson <william.c1.johnson@gmail.com>
2019-03-05 15:48:28 -0500

    Create Fetch-API
    Create a fetch api that will allow us to fetch an endpoint and return a promise of the JSON result or undefined. If an
    error is encountered, the exception modal will be dispatched with the error message.
    
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

---
+++ source/iml/fetch-api.js
@@ -0,0 +1,28 @@
+// @flow
+
+//
+// Copyright (c) 2019 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+import getStore from "./store/get-store.js";
+//import { SHOW_EXCEPTION_MODAL_ACTION } from "./exception/exception-modal-reducer.js";
+
+export default function fetchApi<T>(url: string, options: ?Object): Promise<?T> {
+  options = options || {};
+  options = {
+    method: "GET",
+    ...options
+  };
+
+  const r: Promise<?T> = fetch(url, options)
+    .then(x => x.json())
+    .catch(e => {
+      // getStore.dispatch({
+      //   type: SHOW_EXCEPTION_MODAL_ACTION,
+      //   payload: e
+      // });
+    });
+
+  return r;
+}
