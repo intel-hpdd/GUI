8a45f17c9ad4ab7e1f772bc768cf99cf0affd7b0
Will Johnson <wjohnson@whamcloud.com>
2019-03-31 10:05:54 -0400

    Wait for SSE before loading the app (#193)
    The app should not load until data is received from the event stream. This patch will add a resolve on the sse to make
    sure data is received from the store before the app is loaded.
    
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

--- test/spec/iml/app/app-resolves-test.js
+++ test/spec/iml/app/app-resolves-test.js
@@ -1,31 +1,36 @@
+import highland from "highland";
+
 describe("app resolves", () => {
-  let mockSocketStream, mockResolveStream, promise, stream, appModule, mockCacheInitialData;
+  let mockSocketStream, promise, stream, locksStream, appModule, mockCacheInitialData, mockGetStore;
   beforeEach(() => {
     promise = {};
-    mockResolveStream = jest.fn();
-    mockResolveStream.mockReturnValue(promise);
-    stream = {};
+    stream = highland([promise]);
     mockSocketStream = jest.fn();
     mockSocketStream.mockReturnValue(stream);
     mockCacheInitialData = { session: {} };
-    appModule = jest.mock("../../../../source/iml/promise-transforms.js", () => ({
-      resolveStream: mockResolveStream
-    }));
+    locksStream = highland();
+    mockGetStore = {
+      select: jest.fn(() => locksStream)
+    };
     jest.mock("../../../../source/iml/socket/socket-stream.js", () => mockSocketStream);
     jest.mock("../../../../source/iml/environment.js", () => ({
       CACHE_INITIAL_DATA: mockCacheInitialData
     }));
+    jest.mock("../../../../source/iml/store/get-store.js", () => mockGetStore);
 
     appModule = require("../../../../source/iml/app/app-resolves.js");
   });
 
   describe("app alert stream", () => {
     let result;
-    beforeEach(() => {
-      result = appModule.alertStream();
+    beforeEach(async () => {
+      result = await appModule.alertStream();
     });
-    it("should return a promise", () => {
-      expect(result).toBe(promise);
+    it("should return a promise", done => {
+      result.each(x => {
+        expect(x).toBe(promise);
+        done();
+      });
     });
     it("should create a socket connection", () => {
       expect(mockSocketStream).toHaveBeenCalledOnceWith("/alert/", {
@@ -36,17 +41,17 @@ describe("app resolves", () => {
   });
   describe("app notification stream", () => {
     let result;
-    beforeEach(() => {
-      result = appModule.appNotificationStream();
+    beforeEach(async () => {
+      result = await appModule.appNotificationStream();
     });
-    it("should return a promise", () => {
-      expect(result).toBe(promise);
+    it("should return a promise", done => {
+      result.each(x => {
+        expect(x).toBe(promise);
+        done();
+      });
     });
     it("should create a socket connection", () => {
       expect(mockSocketStream).toHaveBeenCalledOnceWith("/health");
-    });
-    it("should call resolveStream with a stream", () => {
-      expect(mockResolveStream).toHaveBeenCalledOnceWith(stream);
     });
   });
   describe("app session", () => {
@@ -56,6 +61,17 @@ describe("app resolves", () => {
     });
     it("should return the session", () => {
       expect(appSession).toBe(mockCacheInitialData.session);
+    });
+  });
+
+  describe("sseResolves", () => {
+    beforeEach(() => {
+      locksStream.write({});
+    });
+
+    it("should return a Promise containing the lock information", async () => {
+      const locks = await appModule.sseResolves();
+      expect(locks).toEqual({});
     });
   });
 });
