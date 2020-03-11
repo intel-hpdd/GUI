631005293456c39f1add8961f345b2bab4182148
Joe Grund <jgrund@whamcloud.io>
2020-03-11 13:35:04 -0400

    Update wasm_bindgen assignment
    Fixes #237.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- source/iml/global.js
+++ source/iml/global.js
@@ -1,8 +1,13 @@
-// @flow
-
-//
-// Copyright (c) 2018 DDN. All rights reserved.
+// Copyright (c) 2020 DDN. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
+
+try {
+  // Assign directly to window due to https://github.com/rustwasm/wasm-bindgen/pull/1995
+  // eslint-disable-next-line no-undef, camelcase
+  window.wasm_bindgen = wasm_bindgen;
+} catch (e) {
+  window.wasm_bindgen = {};
+}
 
 export default window;
